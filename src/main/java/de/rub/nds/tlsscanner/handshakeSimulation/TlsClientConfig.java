ab346da03fe98a107daa9185afc435efed810615
jankaisr <jan.kaiser-e5q@rub.de>
2018-08-28 15:52:08 +0200

    added basic classes for the handshake simulation

---
+++ src/main/java/de/rub/nds/tlsscanner/handshakeSimulation/TlsClientConfig.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.rub.nds.tlsscanner.handshakeSimulation;
+
+import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TlsClientConfig implements Serializable {
+    
+    private String type;   
+    private String version;
+    private Config config;
+    private WorkflowTrace trace;
+    
+    public void createClientHelloConfig(String type, String version) {
+        this.type = type;
+        this.version = version;
+        this.config = null;
+        this.trace = null;
+    }
+    
+    public void setTrace(WorkflowTrace trace) {
+        this.trace = trace;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+    
+    public WorkflowTrace getTrace() {
+        return trace;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+}
---
+++ src/main/java/de/rub/nds/tlsscanner/handshakeSimulation/TlsClientConfigIO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.rub.nds.tlsscanner.handshakeSimulation;
+
+import de.rub.nds.modifiablevariable.util.XMLPrettyPrinter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXB;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactoryConfigurationException;
+import org.xml.sax.SAXException;
+
+public class TlsClientConfigIO {
+    
+    private static final Logger LOGGER = Logger.getLogger(TlsClientConfigIO.class.getName());
+
+    public TlsClientConfigIO(Level logLevel) {
+        this.LOGGER.setLevel(logLevel);
+        
+    }
+
+    public void writeConfigToFile(TlsClientConfig clientConfig, File configFile) {
+        OutputStream os = null;
+        try {
+            os = new FileOutputStream(configFile);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(TlsClientConfigIO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ByteArrayOutputStream tempStream = new ByteArrayOutputStream();
+        JAXB.marshal(clientConfig, tempStream);
+        try {
+            os.write(XMLPrettyPrinter.prettyPrintXML(new String(tempStream.toByteArray())).getBytes());
+        } catch (IOException | TransformerException | XPathExpressionException | XPathFactoryConfigurationException
+                | ParserConfigurationException | SAXException ex) {
+            throw new RuntimeException("Could not format XML");
+        }
+    }
+    
+    public TlsClientConfig readConfigFromFile(File configFile) {
+        TlsClientConfig clientConfig = JAXB.unmarshal(configFile, TlsClientConfig.class);
+        return clientConfig;
+    }
+    
+    public void removeConfigFile(File configFile) {
+        configFile.delete();
+    }
+}
