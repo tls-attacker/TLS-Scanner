e2594bd6bf42d6aa00fa61e35155c4f14f741ee8
jankaisr <jan.kaiser-e5q@rub.de>
2018-10-08 10:45:08 +0200

    added variables to store rsa and dh min max lengths

--- src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/TlsClientConfig.java
+++ src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/TlsClientConfig.java
@@ -20,22 +20,22 @@ public class TlsClientConfig implements Serializable {
     private String version;
     private Config config;
     private WorkflowTrace trace;
+    private Integer rsaMin;
+    private Integer rsaMax;
+    private Integer dhMin;
+    private Integer dhMax;
     
     public void createClientHelloConfig(String type, String version) {
         this.type = type;
         this.version = version;
         this.config = null;
         this.trace = null;
+        this.rsaMin = null;
+        this.rsaMax = null;
+        this.dhMin = null;
+        this.dhMax = null;
     }
     
-    public void setTrace(WorkflowTrace trace) {
-        this.trace = trace;
-    }
-
-    public void setConfig(Config config) {
-        this.config = config;
-    }
-
     public String getType() {
         return type;
     }
@@ -43,12 +43,52 @@ public class TlsClientConfig implements Serializable {
     public String getVersion() {
         return version;
     }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+    
+    public Config getConfig() {
+        return config;
+    }
+    
+    public void setTrace(WorkflowTrace trace) {
+        this.trace = trace;
+    }
     
     public WorkflowTrace getTrace() {
         return trace;
     }
 
-    public Config getConfig() {
-        return config;
+    public void setRsaMin(Integer rsaMin) {
+        this.rsaMin = rsaMin;
+    }
+
+    public Integer getRsaMin() {
+        return rsaMin;
+    }
+
+    public void setRsaMax(Integer rsaMax) {
+        this.rsaMax = rsaMax;
+    }
+
+    public Integer getRsaMax() {
+        return rsaMax;
+    }
+
+    public void setDhMin(Integer dhMin) {
+        this.dhMin = dhMin;
+    }
+
+    public Integer getDhMin() {
+        return dhMin;
+    }
+
+    public void setDhMax(Integer dhMax) {
+        this.dhMax = dhMax;
+    }
+
+    public Integer getDhMax() {
+        return dhMax;
     }
 }
