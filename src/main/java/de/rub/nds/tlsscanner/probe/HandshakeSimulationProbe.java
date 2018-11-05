62d9425bd4e8da8d7c5cf853f323dc763be7563c
jankaisr <jan.kaiser-e5q@rub.de>
2018-11-05 23:15:45 +0100

    changed code structure and fixed some things

--- src/main/java/de/rub/nds/tlsscanner/probe/HandshakeSimulationProbe.java
+++ src/main/java/de/rub/nds/tlsscanner/probe/HandshakeSimulationProbe.java
@@ -7,13 +7,10 @@ package de.rub.nds.tlsscanner.probe;
 
 import de.rub.nds.modifiablevariable.util.Modifiable;
 import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
-import de.rub.nds.tlsattacker.core.constants.NamedGroup;
-import de.rub.nds.tlsattacker.core.constants.ProtocolMessageType;
 import de.rub.nds.tlsattacker.core.exceptions.WorkflowExecutionException;
 import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
-import de.rub.nds.tlsattacker.core.protocol.message.ECDHEServerKeyExchangeMessage;
-import de.rub.nds.tlsattacker.core.protocol.message.ServerKeyExchangeMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.extension.ExtensionMessage;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.state.TlsContext;
@@ -94,95 +91,121 @@ public class HandshakeSimulationProbe extends TlsProbe {
     }
 
     private void evaluateClient(TlsClientConfig clientConfig, State state, SimulatedClient simulatedClient) {
-        WorkflowTrace trace = state.getWorkflowTrace();
         TlsContext context = state.getTlsContext();
-        try {
-            simulatedClient.setReceivedServerHello(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_HELLO, trace));
-            simulatedClient.setHighestClientProtocolVersion(context.getHighestClientProtocolVersion());
-            if (simulatedClient.getReceivedServerHello()) {
-                simulatedClient.setSelectedProtocolVersion(context.getSelectedProtocolVersion());
-                simulatedClient.setSelectedCiphersuite(context.getSelectedCipherSuite());
-                if (simulatedClient.getSelectedCiphersuite().toString().contains("_DHE_")
-                        || simulatedClient.getSelectedCiphersuite().toString().contains("_ECDHE_")) {
-                    simulatedClient.setForwardSecrecy(true);
-                } else {
-                    simulatedClient.setForwardSecrecy(false);
-                }
-                simulatedClient.setSupportedRsaKeyLengthList(clientConfig.getSupportedRsaKeyLengthList());
-                simulatedClient.setSupportedDheKeyLengthList(clientConfig.getSupportedDheKeyLengthList());
-                simulatedClient.setSelectedCompressionMethod(context.getSelectedCompressionMethod());
-                if (context.getNegotiatedExtensionSet() != null && !context.getNegotiatedExtensionSet().isEmpty()) {
-                    simulatedClient.setNegotiatedExtensions(context.getNegotiatedExtensionSet().toString());
-                }
-                if (clientConfig.getConfig().isAddAlpnExtension()) {
-                    simulatedClient.setAlpnAnnouncedProtocols(Arrays.toString(clientConfig.getConfig().getAlpnAnnouncedProtocols()));
-                } else {
-                    simulatedClient.setAlpnAnnouncedProtocols("-");
-                }
+        evaluateClientConfig(clientConfig, simulatedClient);
+        evaluateReceivedMessages(state, simulatedClient);
+        if (simulatedClient.getReceivedServerHello()) {
+            evaluateServerHello(context, simulatedClient);
+        }
+        if (simulatedClient.getReceivedCertificate()) {
+            evaluateCertificate(context, simulatedClient);
+        }
+        if (simulatedClient.getReceivedServerKeyExchange()) {
+            evaluateServerKeyExchange(context, simulatedClient);
+        }
+        if (simulatedClient.getReceivedServerHelloDone()) {
+            evaluateServerHelloDone(context, simulatedClient);
+        } else {
+            simulatedClient.setHandshakeSuccessful(false);
+            simulatedClient.setHandshakeFailedBecause("Server did not send required message: ServerHelloDone");
+            if (!simulatedClient.getReceivedServerHello()) {
+                simulatedClient.setHandshakeFailedBecause("Server did not send required messages: ServerHello, ServerHelloDone");
             }
-            simulatedClient.setReceivedCertificate(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.CERTIFICATE, trace));
-            if (simulatedClient.getReceivedCertificate()) {
-                if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_RSA")
-                        && context.getServerRSAPublicKey() != null) {
-                    simulatedClient.setServerPublicKeyLength(getPublicKeyFromCert(context.getServerCertificate(), "TLS_RSA"));
-                }
-            }
-            simulatedClient.setReceivedServerKeyExchange(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_KEY_EXCHANGE, trace));
-            if (simulatedClient.getReceivedServerKeyExchange()) {
-                if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_DH")
-                        && context.getServerDhPublicKey() != null) {
-                    simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerDhPublicKey().bitLength()));
-                } else if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_ECDH")
-                        && context.getServerEcPublicKey() != null) {
-                    simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerEcPublicKey().getByteX().length * 8));
-                    simulatedClient.setSelectedNamedGroup(context.getSelectedGroup().name());
-                } else if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_PSK")
-                        && context.getServerPSKPublicKey() != null) {
-                    simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerPSKPublicKey().bitLength()));
-                } else if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_SRP")
-                        && context.getServerSRPPublicKey() != null) {
-                    simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerSRPPublicKey().bitLength()));
-                } else if (simulatedClient.getSelectedCiphersuite().usesGOSTR3411()
-                        && context.getServerGostEc01PublicKey() != null) {
-                    simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerGostEc01PublicKey().getByteX().length * 8));
-                }
-            }
-            simulatedClient.setReceivedCertificateRequest(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.CERTIFICATE_REQUEST, trace));
-            simulatedClient.setReceivedServerHelloDone(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_HELLO_DONE, trace));
-            if (simulatedClient.getReceivedServerHelloDone()) {
-                simulatedClient.setHandshakeSuccessful(true);
-                simulatedClient.setServerPublicKeyLengthAccept(true);
-                if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_RSA")
-                        && simulatedClient.getSupportedRsaKeyLengthList() != null
-                        && !simulatedClient.getSupportedRsaKeyLengthList().contains(
-                                Integer.parseInt(simulatedClient.getServerPublicKeyLength()))) {
-                    simulatedClient.setHandshakeSuccessful(false);
-                    simulatedClient.setServerPublicKeyLengthAccept(false);
-                }
-                if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_DHE_RSA")
-                        && simulatedClient.getSupportedDheKeyLengthList() != null
-                        && !simulatedClient.getSupportedDheKeyLengthList().contains(
-                                Integer.parseInt(simulatedClient.getServerPublicKeyLength()))) {
-                    simulatedClient.setHandshakeSuccessful(false);
-                    simulatedClient.setServerPublicKeyLengthAccept(false);
-                }
-                if (simulatedClient.getServerPublicKeyLengthAccept() == false) {
-                    simulatedClient.setHandshakeFailedBecause("Server public key length ("
-                            + simulatedClient.getServerPublicKeyLength() + ") probably not supported by client");
-                }
-            } else {
-                simulatedClient.setHandshakeSuccessful(false);
-                simulatedClient.setHandshakeFailedBecause("Server did not send message: ServerHelloDone");
-                if (!simulatedClient.getReceivedServerHello()) {
-                    simulatedClient.setHandshakeFailedBecause("Server did not send messages: ServerHello, ServerHelloDone");
-                }
-            }            
-            simulatedClientList.add(simulatedClient);
-        } catch (Exception ex) {
-            throw new RuntimeException(clientConfig.getType() + ":" + clientConfig.getVersion(), ex);
+        }
+        simulatedClientList.add(simulatedClient);
+    }
+
+    private void evaluateClientConfig(TlsClientConfig clientConfig, SimulatedClient simulatedClient) {
+        simulatedClient.setSupportedRsaKeyLengthList(clientConfig.getSupportedRsaKeyLengthList());
+        simulatedClient.setSupportedDheKeyLengthList(clientConfig.getSupportedDheKeyLengthList());
+        simulatedClient.setHighestClientProtocolVersion(clientConfig.getConfig().getHighestProtocolVersion());
+        if (clientConfig.getConfig().isAddAlpnExtension()) {
+            simulatedClient.setAlpnAnnouncedProtocols(Arrays.toString(clientConfig.getConfig().getAlpnAnnouncedProtocols()));
+        } else {
+            simulatedClient.setAlpnAnnouncedProtocols("-");
         }
     }
-    
+
+    private void evaluateReceivedMessages(State state, SimulatedClient simulatedClient) {
+        WorkflowTrace trace = state.getWorkflowTrace();
+        simulatedClient.setReceivedServerHello(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_HELLO, trace));
+        simulatedClient.setReceivedCertificate(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.CERTIFICATE, trace));
+        simulatedClient.setReceivedServerKeyExchange(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_KEY_EXCHANGE, trace));
+        simulatedClient.setReceivedCertificateRequest(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.CERTIFICATE_REQUEST, trace));
+        simulatedClient.setReceivedServerHelloDone(WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_HELLO_DONE, trace));
+    }
+
+    private void evaluateServerHello(TlsContext context, SimulatedClient simulatedClient) {
+        simulatedClient.setSelectedProtocolVersion(context.getSelectedProtocolVersion());
+        CipherSuite cipherSuite = context.getSelectedCipherSuite();
+        simulatedClient.setSelectedCiphersuite(cipherSuite);
+        if (cipherSuite.toString().contains("_DHE_") || cipherSuite.toString().contains("_ECDHE_")) {
+            simulatedClient.setForwardSecrecy(true);
+        } else {
+            simulatedClient.setForwardSecrecy(false);
+        }
+        simulatedClient.setSelectedCompressionMethod(context.getSelectedCompressionMethod());
+        if (context.getNegotiatedExtensionSet() != null) {
+            simulatedClient.setNegotiatedExtensions(context.getNegotiatedExtensionSet().toString());
+        }
+    }
+
+    private void evaluateCertificate(TlsContext context, SimulatedClient simulatedClient) {
+        if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_RSA")) {
+            simulatedClient.setServerPublicKeyLength(getPublicKeyFromCert(context.getServerCertificate(), "TLS_RSA"));
+        }
+    }
+
+    private void evaluateServerKeyExchange(TlsContext context, SimulatedClient simulatedClient) {
+        CipherSuite cipherSuite = context.getSelectedCipherSuite();
+        if (cipherSuite.name().contains("TLS_DH") && context.getServerDhPublicKey() != null) {
+            simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerDhPublicKey().bitLength()));
+        } else if (cipherSuite.name().contains("TLS_ECDH")) {
+            if (context.getSelectedGroup() != null) {
+                simulatedClient.setSelectedNamedGroup(context.getSelectedGroup().name());
+                if (context.getSelectedGroup().getCoordinateSizeInBit() != null) {
+                    simulatedClient.setServerPublicKeyLength(context.getSelectedGroup().getCoordinateSizeInBit().toString());
+                }
+            }
+            if (simulatedClient.getServerPublicKeyLength() == null) {
+                if (context.getServerEcPublicKey() != null) {
+                    simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerEcPublicKey().getByteX().length * 8));
+                } else {
+                    simulatedClient.setServerPublicKeyLength("according to named group: " + simulatedClient.getSelectedNamedGroup());
+                }
+            }
+        } else if (cipherSuite.name().contains("TLS_PSK") && context.getServerPSKPublicKey() != null) {
+            simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerPSKPublicKey().bitLength()));
+        } else if (cipherSuite.name().contains("TLS_SRP") && context.getServerSRPPublicKey() != null) {
+            simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerSRPPublicKey().bitLength()));
+        } else if (cipherSuite.usesGOSTR3411() && context.getServerGostEc01PublicKey() != null) {
+            simulatedClient.setServerPublicKeyLength(Integer.toString(context.getServerGostEc01PublicKey().getByteX().length * 8));
+        }
+        if (!cipherSuite.name().contains("TLS_ECDH")) {
+            simulatedClient.setSelectedNamedGroup("-");
+        }
+    }
+
+    private void evaluateServerHelloDone(TlsContext context, SimulatedClient simulatedClient) {
+        simulatedClient.setHandshakeSuccessful(true);
+        simulatedClient.setServerPublicKeyLengthAccept(true);
+        if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_RSA")
+                && simulatedClient.getSupportedRsaKeyLengthList() != null
+                && !simulatedClient.getSupportedRsaKeyLengthList().contains(Integer.parseInt(simulatedClient.getServerPublicKeyLength()))) {
+            simulatedClient.setServerPublicKeyLengthAccept(false);
+        }
+        if (simulatedClient.getSelectedCiphersuite().name().contains("TLS_DHE_RSA")
+                && simulatedClient.getSupportedDheKeyLengthList() != null
+                && !simulatedClient.getSupportedDheKeyLengthList().contains(Integer.parseInt(simulatedClient.getServerPublicKeyLength()))) {
+            simulatedClient.setServerPublicKeyLengthAccept(false);
+        }
+        if (simulatedClient.getServerPublicKeyLengthAccept() == false) {
+            simulatedClient.setHandshakeSuccessful(false);
+            simulatedClient.setHandshakeFailedBecause("Server public key length ("
+                    + simulatedClient.getServerPublicKeyLength() + ") probably not supported by client");
+        }
+    }
+
     private String getPublicKeyFromCert(Certificate certs, String algo) {
         try {
             if (certs != null) {
@@ -191,7 +214,6 @@ public class HandshakeSimulationProbe extends TlsProbe {
                     if (x509Cert.getPublicKey() != null) {
                         if (algo.contains("RSA")) {
                             RSAPublicKey rsaPk = (RSAPublicKey) x509Cert.getPublicKey();
-                            System.out.println(rsaPk.getModulus().bitLength());
                             return Integer.toString(rsaPk.getModulus().bitLength());
                         }
                     }
