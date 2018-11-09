e981b4a369f26f49bed0dc790d71ec8228dbd6af
jankaisr <jan.kaiser-e5q@rub.de>
2018-11-09 16:15:08 +0100

    changed variables

--- src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/SimulatedClient.java
+++ src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/SimulatedClient.java
@@ -8,44 +8,52 @@ package de.rub.nds.tlsscanner.probe.handshakeSimulation;
 import de.rub.nds.tlsattacker.core.constants.CipherSuite;
 import de.rub.nds.tlsattacker.core.constants.CompressionMethod;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SimulatedClient {
 
     private final String type;
     private final String version;
-    private final Boolean defaultVersion;
+    private final boolean defaultVersion;
+    //To set in HandshakeSimulationProbe
     private Boolean receivedServerHello = null;
-    private Boolean handshakeSuccessful = null;
-    private String handshakeFailedBecause = null;
+    private Boolean receivedCertificate = null;
+    private Boolean receivedServerKeyExchange = null;
+    private Boolean receivedCertificateRequest = null;
+    private Boolean receivedServerHelloDone = null;
     private ProtocolVersion highestClientProtocolVersion = null;
     private ProtocolVersion selectedProtocolVersion = null;
-    private Boolean highestPossibleProtocolVersionSeleceted = null;
+    private List<CipherSuite> clientSupportedCiphersuites = null;
     private CipherSuite selectedCiphersuite = null;
     private Boolean forwardSecrecy = null;
-    private List<Integer> supportedRsaKeyLengthList = null;
-    private List<Integer> supportedDheKeyLengthList = null;
     private CompressionMethod selectedCompressionMethod = null;
     private String negotiatedExtensions = null;
     private String alpnAnnouncedProtocols = null;
-    private Boolean receivedCertificate = null;
-    private Boolean receivedServerKeyExchange = null;
     private String selectedNamedGroup = null;
     private String serverPublicKeyLength = null;
-    private Boolean serverPublicKeyLengthAccept = null;
-    private Boolean receivedCertificateRequest = null;
-    private Boolean receivedServerHelloDone = null;
-    private Boolean connectionSecure = null;
-    private String connectionInsecureBecause = null;
+    private List<ProtocolVersion> supportedVersionList = null;
+    private List<ProtocolVersion> versionAcceptForbiddenCiphersuiteList = null;
+    private List<Integer> supportedRsaKeyLengthList = null;
+    private List<Integer> supportedDheKeyLengthList = null;
+    //To set in HandshakeSimulationAfterProbe
+    private Boolean highestPossibleProtocolVersionSeleceted = null;
+    private Boolean handshakeSuccessful = null;
     private Boolean paddingOracleVulnerable = null;
     private Boolean bleichenbacherVulnerable = null;
     private Boolean crimeVulnerable = null;
     private Boolean sweet32Vulnerable = null;
-    
-    public SimulatedClient(String type, String version, Boolean defaultVersion) {
+    private Boolean connectionInsecure = null;
+    private Boolean connectionRfc7918Secure = null;
+    private List<HandshakeFailed> failReasons = null;
+    private List<HandshakeInsecure> insecureReasons = null;
+
+    public SimulatedClient(String type, String version, boolean defaultVersion) {
         this.type = type;
         this.version = version;
         this.defaultVersion = defaultVersion;
+        this.failReasons = new LinkedList<>();
+        this.insecureReasons = new LinkedList<>();
     }
 
     public String getType() {
@@ -56,34 +64,50 @@ public class SimulatedClient {
         return version;
     }
 
-    public Boolean isDefaultVersion() {
+    public boolean isDefaultVersion() {
         return defaultVersion;
     }
-    
+
     public Boolean getReceivedServerHello() {
         return receivedServerHello;
     }
-    
+
     public void setReceivedServerHello(Boolean receivedServerHello) {
         this.receivedServerHello = receivedServerHello;
     }
 
-    public Boolean getHandshakeSuccessful() {
-        return handshakeSuccessful;
+    public Boolean getReceivedCertificate() {
+        return receivedCertificate;
     }
 
-    public void setHandshakeSuccessful(Boolean handshakeSuccessful) {
-        this.handshakeSuccessful = handshakeSuccessful;
+    public void setReceivedCertificate(Boolean receivedCertificate) {
+        this.receivedCertificate = receivedCertificate;
     }
 
-    public String getHandshakeFailedBecause() {
-        return handshakeFailedBecause;
+    public Boolean getReceivedServerKeyExchange() {
+        return receivedServerKeyExchange;
     }
 
-    public void setHandshakeFailedBecause(String handshakeFailedBecause) {
-        this.handshakeFailedBecause = handshakeFailedBecause;
+    public void setReceivedServerKeyExchange(Boolean receivedServerKeyExchange) {
+        this.receivedServerKeyExchange = receivedServerKeyExchange;
     }
-    
+
+    public Boolean getReceivedCertificateRequest() {
+        return receivedCertificateRequest;
+    }
+
+    public void setReceivedCertificateRequest(Boolean receivedCertificateRequest) {
+        this.receivedCertificateRequest = receivedCertificateRequest;
+    }
+
+    public Boolean getReceivedServerHelloDone() {
+        return receivedServerHelloDone;
+    }
+
+    public void setReceivedServerHelloDone(Boolean receivedServerHelloDone) {
+        this.receivedServerHelloDone = receivedServerHelloDone;
+    }
+
     public ProtocolVersion getHighestClientProtocolVersion() {
         return highestClientProtocolVersion;
     }
@@ -91,15 +115,23 @@ public class SimulatedClient {
     public void setHighestClientProtocolVersion(ProtocolVersion highestClientProtocolVersion) {
         this.highestClientProtocolVersion = highestClientProtocolVersion;
     }
-    
+
     public ProtocolVersion getSelectedProtocolVersion() {
         return selectedProtocolVersion;
     }
-    
+
     public void setSelectedProtocolVersion(ProtocolVersion selectedProtocolVersion) {
         this.selectedProtocolVersion = selectedProtocolVersion;
     }
-    
+
+    public List<CipherSuite> getClientSupportedCiphersuites() {
+        return clientSupportedCiphersuites;
+    }
+
+    public void setClientSupportedCiphersuites(List<CipherSuite> clientSupportedCiphersuites) {
+        this.clientSupportedCiphersuites = clientSupportedCiphersuites;
+    }
+
     public Boolean getHighestPossibleProtocolVersionSeleceted() {
         return highestPossibleProtocolVersionSeleceted;
     }
@@ -107,7 +139,7 @@ public class SimulatedClient {
     public void setHighestPossibleProtocolVersionSeleceted(Boolean highestPossibleProtocolVersionSeleceted) {
         this.highestPossibleProtocolVersionSeleceted = highestPossibleProtocolVersionSeleceted;
     }
-    
+
     public CipherSuite getSelectedCiphersuite() {
         return selectedCiphersuite;
     }
@@ -115,13 +147,69 @@ public class SimulatedClient {
     public void setSelectedCiphersuite(CipherSuite selectedCiphersuite) {
         this.selectedCiphersuite = selectedCiphersuite;
     }
-    
+
     public Boolean getForwardSecrecy() {
         return forwardSecrecy;
     }
-    
+
     public void setForwardSecrecy(Boolean forwardSecrecy) {
         this.forwardSecrecy = forwardSecrecy;
+    }
+
+    public CompressionMethod getSelectedCompressionMethod() {
+        return selectedCompressionMethod;
+    }
+
+    public void setSelectedCompressionMethod(CompressionMethod selectedCompressionMethod) {
+        this.selectedCompressionMethod = selectedCompressionMethod;
+    }
+
+    public String getNegotiatedExtensions() {
+        return negotiatedExtensions;
+    }
+
+    public void setNegotiatedExtensions(String negotiatedExtensions) {
+        this.negotiatedExtensions = negotiatedExtensions;
+    }
+
+    public String getAlpnAnnouncedProtocols() {
+        return alpnAnnouncedProtocols;
+    }
+
+    public void setAlpnAnnouncedProtocols(String alpnAnnouncedProtocols) {
+        this.alpnAnnouncedProtocols = alpnAnnouncedProtocols;
+    }
+
+    public String getSelectedNamedGroup() {
+        return selectedNamedGroup;
+    }
+
+    public void setSelectedNamedGroup(String selectedNamedGroup) {
+        this.selectedNamedGroup = selectedNamedGroup;
+    }
+
+    public String getServerPublicKeyLength() {
+        return serverPublicKeyLength;
+    }
+
+    public void setServerPublicKeyLength(String serverPublicKeyLength) {
+        this.serverPublicKeyLength = serverPublicKeyLength;
+    }
+
+    public List<ProtocolVersion> getSupportedVersionList() {
+        return supportedVersionList;
+    }
+
+    public void setSupportedVersionList(List<ProtocolVersion> supportedVersionList) {
+        this.supportedVersionList = supportedVersionList;
+    }
+
+    public List<ProtocolVersion> getVersionAcceptForbiddenCiphersuiteList() {
+        return versionAcceptForbiddenCiphersuiteList;
+    }
+
+    public void setVersionAcceptForbiddenCiphersuiteList(List<ProtocolVersion> versionAcceptForbiddenCiphersuiteList) {
+        this.versionAcceptForbiddenCiphersuiteList = versionAcceptForbiddenCiphersuiteList;
     }
 
     public List<Integer> getSupportedRsaKeyLengthList() {
@@ -139,103 +227,15 @@ public class SimulatedClient {
     public void setSupportedDheKeyLengthList(List<Integer> supportedDheKeyLengthList) {
         this.supportedDheKeyLengthList = supportedDheKeyLengthList;
     }
-    
-    public CompressionMethod getSelectedCompressionMethod() {
-        return selectedCompressionMethod;
+
+    public Boolean getHandshakeSuccessful() {
+        return handshakeSuccessful;
     }
 
-    public void setSelectedCompressionMethod(CompressionMethod selectedCompressionMethod) {
-        this.selectedCompressionMethod = selectedCompressionMethod;
-    }
-    
-    public String getNegotiatedExtensions() {
-        return negotiatedExtensions;
+    public void setHandshakeSuccessful(Boolean handshakeSuccessful) {
+        this.handshakeSuccessful = handshakeSuccessful;
     }
 
-    public void setNegotiatedExtensions(String negotiatedExtensions) {
-        this.negotiatedExtensions = negotiatedExtensions;
-    }
-
-    public String getAlpnAnnouncedProtocols() {
-        return alpnAnnouncedProtocols;
-    }
-
-    public void setAlpnAnnouncedProtocols(String alpnAnnouncedProtocols) {
-        this.alpnAnnouncedProtocols = alpnAnnouncedProtocols;
-    }
-    
-    public Boolean getReceivedCertificate() {
-        return receivedCertificate;
-    }
-
-    public void setReceivedCertificate(Boolean receivedCertificate) {
-        this.receivedCertificate = receivedCertificate;
-    }
-    
-    public Boolean getReceivedServerKeyExchange() {
-        return receivedServerKeyExchange;
-    }
-
-    public void setReceivedServerKeyExchange(Boolean receivedServerKeyExchange) {
-        this.receivedServerKeyExchange = receivedServerKeyExchange;
-    }
-    
-    public String getSelectedNamedGroup() {
-        return selectedNamedGroup;
-    }
-
-    public void setSelectedNamedGroup(String selectedNamedGroup) {
-        this.selectedNamedGroup = selectedNamedGroup;
-    }
-    
-    public String getServerPublicKeyLength() {
-        return serverPublicKeyLength;
-    }
-
-    public void setServerPublicKeyLength(String serverPublicKeyLength) {
-        this.serverPublicKeyLength = serverPublicKeyLength;
-    }
-
-    public Boolean getServerPublicKeyLengthAccept() {
-        return serverPublicKeyLengthAccept;
-    }
-
-    public void setServerPublicKeyLengthAccept(Boolean serverPublicKeyLengthCheck) {
-        this.serverPublicKeyLengthAccept = serverPublicKeyLengthCheck;
-    }
-    
-    public Boolean getReceivedCertificateRequest() {
-        return receivedCertificateRequest;
-    }
-
-    public void setReceivedCertificateRequest(Boolean receivedCertificateRequest) {
-        this.receivedCertificateRequest = receivedCertificateRequest;
-    }
-    
-    public Boolean getReceivedServerHelloDone() {
-        return receivedServerHelloDone;
-    }
-    
-    public void setReceivedServerHelloDone(Boolean receivedServerHelloDone) {
-        this.receivedServerHelloDone = receivedServerHelloDone;
-    }
-
-    public Boolean getConnectionSecure() {
-        return connectionSecure;
-    }
-
-    public void setConnectionSecure(Boolean connectionSecure) {
-        this.connectionSecure = connectionSecure;
-    }
-
-    public String getConnectionInsecureBecause() {
-        return connectionInsecureBecause;
-    }
-
-    public void setConnectionInsecureBecause(String connectionInsecureBecause) {
-        this.connectionInsecureBecause = connectionInsecureBecause;
-    }
-    
     public Boolean getPaddingOracleVulnerable() {
         return paddingOracleVulnerable;
     }
@@ -251,7 +251,7 @@ public class SimulatedClient {
     public void setBleichenbacherVulnerable(Boolean bleichenbacherVulnerable) {
         this.bleichenbacherVulnerable = bleichenbacherVulnerable;
     }
-    
+
     public Boolean getCrimeVulnerable() {
         return crimeVulnerable;
     }
@@ -266,5 +266,37 @@ public class SimulatedClient {
 
     public void setSweet32Vulnerable(Boolean sweet32Vulnerable) {
         this.sweet32Vulnerable = sweet32Vulnerable;
+    }
+
+    public Boolean getConnectionInsecure() {
+        return connectionInsecure;
+    }
+
+    public void setConnectionInsecure(Boolean connectionInsecure) {
+        this.connectionInsecure = connectionInsecure;
+    }
+
+    public Boolean getConnectionRfc7918Secure() {
+        return connectionRfc7918Secure;
+    }
+
+    public void setConnectionRfc7918Secure(Boolean connectionRfc7918Secure) {
+        this.connectionRfc7918Secure = connectionRfc7918Secure;
+    }
+
+    public List<HandshakeFailed> getFailReasons() {
+        return failReasons;
+    }
+
+    public void addToFailReasons(HandshakeFailed handshakeIssue) {
+        failReasons.add(handshakeIssue);
+    }
+
+    public List<HandshakeInsecure> getInsecureReasons() {
+        return insecureReasons;
+    }
+
+    public void addToInsecureReasons(HandshakeInsecure handshakeIssue) {
+        insecureReasons.add(handshakeIssue);
     }
 }
