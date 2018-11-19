653fe548a3fbd47e46fd2235a1728e29c8ae3444
jankaisr <jan.kaiser-e5q@rub.de>
2018-11-19 21:40:39 +0100

    changed some handshake failed reasons

--- src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/HandshakeFailed.java
+++ src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/HandshakeFailed.java
@@ -8,8 +8,8 @@ package de.rub.nds.tlsscanner.probe.handshakeSimulation;
 public enum HandshakeFailed {
     PROTOCOL_MISMATCH ("client and server do not speak the same tls protocol"),
     CIPHERSUITE_MISMATCH ("client and server do not have a single ciphersuite in common"),
-    CIPHERSUITE_FORBIDDEN ("server sent forbidden ciphersuite which the client would not accept"),
-    PUBLIC_KEY_LENGTH_NOT_ACCEPTED ("server sent a public key which the client would not accept because of its length"),
+    CIPHERSUITE_FORBIDDEN ("server sent forbidden ciphersuite which the client does not accept"),
+    PUBLIC_KEY_LENGTH_NOT_ACCEPTED ("server sent a public key which length the client does not accept"),
     UNKNOWN ("reason can not be specified");
     
     private final String reason;
