04469a00f264eb64c82f6d81a252755e7bea6eb4
jankaisr <jan.kaiser-e5q@rub.de>
2018-12-17 20:17:30 +0100

    changed text for protocol mismatch

--- src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/HandshakeFailed.java
+++ src/main/java/de/rub/nds/tlsscanner/probe/handshakeSimulation/HandshakeFailed.java
@@ -6,7 +6,7 @@
 package de.rub.nds.tlsscanner.probe.handshakeSimulation;
 
 public enum HandshakeFailed {
-    PROTOCOL_MISMATCH ("client and server do not speak the same tls protocol"),
+    PROTOCOL_MISMATCH ("client and server do not support a common tls protocol version"),
     CIPHERSUITE_MISMATCH ("client and server do not have a single ciphersuite in common"),
     CIPHERSUITE_FORBIDDEN ("client does not support forbidden ciphersuites for the selected protocol version"),
     PUBLIC_KEY_LENGTH_RSA_NOT_ACCEPTED ("client does not support the length of the rsa public key of the server"),
