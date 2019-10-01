f8695cc8d00d8e6b0387a7370d96975a43589a45
Robert Merget <robert.merget@rub.de>
2019-10-01 17:12:16 +0200

    Reduced impact of breach

--- src/test/java/de/rub/nds/tlsscanner/rating/DefaultInfluencersTest.java
+++ src/test/java/de/rub/nds/tlsscanner/rating/DefaultInfluencersTest.java
@@ -327,7 +327,7 @@ public class DefaultInfluencersTest {
                 new PropertyResultRatingInfluencer(TestResult.TRUE, -500, 1000),
                 new PropertyResultRatingInfluencer(TestResult.FALSE, 0)));
         influencers.add(new RatingInfluencer(AnalyzedProperty.VULNERABLE_TO_BREACH,
-                new PropertyResultRatingInfluencer(TestResult.TRUE, -500, 1000),
+                new PropertyResultRatingInfluencer(TestResult.TRUE, -200, 0),
                 new PropertyResultRatingInfluencer(TestResult.FALSE, 0)));
         influencers.add(new RatingInfluencer(AnalyzedProperty.VULNERABLE_TO_LOGJAM,
                 new PropertyResultRatingInfluencer(TestResult.TRUE, -500, 1000),
