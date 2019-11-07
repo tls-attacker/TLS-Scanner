c1b46d6ab7bf52c037e0e0ff7a5d94027a53e73d
Robert Merget <robert.merget@rub.de>
2019-11-07 16:48:22 +0100

    fixed helloretryrequest random eval bug

--- src/main/java/de/rub/nds/tlsscanner/report/after/EvaluateRandomnessAfterProbe.java
+++ src/main/java/de/rub/nds/tlsscanner/report/after/EvaluateRandomnessAfterProbe.java
@@ -8,18 +8,35 @@
  */
 package de.rub.nds.tlsscanner.report.after;
 
+import de.rub.nds.modifiablevariable.util.ArrayConverter;
+import de.rub.nds.tlsscanner.probe.stats.ComparableByteArray;
 import de.rub.nds.tlsscanner.probe.stats.ExtractedValueContainer;
 import de.rub.nds.tlsscanner.probe.stats.TrackableValueType;
 import de.rub.nds.tlsscanner.report.SiteReport;
 import de.rub.nds.tlsscanner.report.result.statistics.RandomEvaluationResult;
+import java.util.Arrays;
+import java.util.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EvaluateRandomnessAfterProbe extends AfterProbe {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private final static byte[] HELLO_RETRY_REQUEST_CONST = ArrayConverter.hexStringToByteArray("CF21AD74E59A6111BE1D8C021E65B891C2A211167ABB8C5E079E09E2C8A8339C");
 
     @Override
     public void analyze(SiteReport report) {
         ExtractedValueContainer container = report.getExtractedValueContainerMap().get(TrackableValueType.RANDOM);
+        ExtractedValueContainer tempContainter = new ExtractedValueContainer(TrackableValueType.RANDOM);
+        for (Object o : container.getExtractedValueList()) {
+            ComparableByteArray random = (ComparableByteArray) o;
+            if (!Arrays.equals(HELLO_RETRY_REQUEST_CONST, random.getArray())) {
+                tempContainter.put(o);
+            }
+        }
         RandomEvaluationResult result = RandomEvaluationResult.NOT_ANALYZED;
-        if (!container.areAllValuesDiffernt()) {
+        if (!tempContainter.areAllValuesDiffernt()) {
             result = RandomEvaluationResult.DUPLICATES;
         } else {
             result = RandomEvaluationResult.NO_DUPLICATES;
