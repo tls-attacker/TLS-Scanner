b3c3df65cfed49751467ff8fab9b132fd9edfaff
Mario <mario.korth@rub.de>
2019-12-28 14:39:33 +0100

    key format change, new keys

--- keys/genkey.sh
+++ keys/genkey.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 rm *.pem
-openssl genrsa -out keys/ca_key.pem 2048;
+#openssl genrsa -out keys/ca_key.pem 2048;
 for i in `seq 1 10`; do
-    openssl genrsa -out ${i}_key.pem 2048;
+    openssl genrsa -out rsakey_${i}.pem 2048;
+done;
+
+for i in `seq 1 10`; do
+  openssl dhparam -out dhparam_${i}.pem 1024;
+  openssl genpkey -paramfile dhparam_${i}.pem -out dhkey_${i}.pem;
+  openssl pkey -in dhkey_${i}.pem -pubout -out dhpubkey_${i}.pem;
 done;
