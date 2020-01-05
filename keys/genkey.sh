4c6c9c1bc1432e467a4da1030be0a45016ca9725
Mario Korth <mario.korth@rub.de>
2020-01-05 17:12:49 +0100

    new keys, XML definition for v1/v3 Root CA to be generated with X509 Attacker

--- keys/genkey.sh
+++ keys/genkey.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-for i in `seq 1 10`; do
-    openssl genrsa -out rsakey_${i}.pem 2048;
+for i in `seq 0 10`; do
+  openssl genrsa -out rsakey_${i}.pem 2048;
 done;
 
-for i in `seq 1 10`; do
+for i in `seq 0 10`; do
   openssl dhparam -out dhparam_${i}.pem 1024;
   openssl genpkey -paramfile dhparam_${i}.pem -out dhkey_${i}.pem;
   openssl pkey -in dhkey_${i}.pem -pubout -out dhpubkey_${i}.pem;
+done;
+
+for i in `seq 0 10`; do
+  openssl dsaparam -out dsaparam_${i}.pem 2048;
+  openssl gendsa -out dsakey_${i}.pem dsaparam_${i}.pem;
+done;
+
+for named_curve in secp160k1 secp160r1 secp160r2 secp192k1 secp224k1 secp224r1 secp256k1 secp384r1 secp521r1 sect163k1 sect163r1 sect163r2 sect193r1 sect193r2 sect233k1 sect233r1 sect239k1 sect283k1 sect283r1 sect409k1 sect409r1 sect571k1 sect571r1; do
+  openssl ecparam -out ecparam_${named_curve}.pem -name ${named_curve};
+  openssl genpkey -paramfile ecparam_${named_curve}.pem -out eckey_${named_curve}.pem;
+  openssl pkey -in eckey_${named_curve}.pem -pubout -out ecpubkey_${named_curve}.pem;
 done;
