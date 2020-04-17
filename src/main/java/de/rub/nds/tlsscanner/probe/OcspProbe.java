9a4234ef32b0b7ec268259f002832878f159e231
Nirusu <Nirusu@users.noreply.github.com>
2020-04-17 18:18:38 +0200

    Add stub for upcoming OCSP probe

---
+++ src/main/java/de/rub/nds/tlsscanner/probe/OcspProbe.java
@@ -0,0 +1,84 @@
+/**
+ * TLS-Scanner - A TLS configuration and analysis tool based on TLS-Attacker.
+ *
+ * Copyright 2017-2019 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsscanner.probe;
+
+import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.constants.NamedGroup;
+import de.rub.nds.tlsattacker.core.constants.SignatureAndHashAlgorithm;
+import de.rub.nds.tlsattacker.core.util.CertificateFetcher;
+import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
+import de.rub.nds.tlsscanner.config.ScannerConfig;
+import de.rub.nds.tlsscanner.constants.ProbeType;
+import de.rub.nds.tlsscanner.report.SiteReport;
+import de.rub.nds.tlsscanner.report.result.CertificateResult;
+import de.rub.nds.tlsscanner.report.result.OcspResult;
+import de.rub.nds.tlsscanner.report.result.ProbeResult;
+import org.bouncycastle.crypto.tls.Certificate;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Nils Hanke - nils.hanke@rub.de
+ */
+public class OcspProbe extends TlsProbe {
+
+    public OcspProbe(ScannerConfig config, ParallelExecutor parallelExecutor) {
+        super(parallelExecutor, ProbeType.OCSP, config, 0);
+    }
+
+    @Override
+    public ProbeResult executeTest() {
+        Config tlsConfig = initTlsConfig();
+        Certificate serverCert = CertificateFetcher.fetchServerCertificate(tlsConfig);
+        return new OcspResult();
+    }
+
+    private Config initTlsConfig() {
+        Config tlsConfig = getScannerConfig().createConfig();
+        tlsConfig.setQuickReceive(true);
+        tlsConfig.setEarlyStop(true);
+        tlsConfig.setWorkflowTraceType(WorkflowTraceType.HELLO);
+        tlsConfig.setAddSignatureAndHashAlgorithmsExtension(true);
+        tlsConfig.setAddServerNameIndicationExtension(true);
+        tlsConfig.setAddECPointFormatExtension(true);
+        tlsConfig.setAddEllipticCurveExtension(true);
+        tlsConfig.setAddCertificateStatusRequestExtension(true);
+
+        List<CipherSuite> toTestList = new LinkedList<>();
+        toTestList.addAll(Arrays.asList(CipherSuite.values()));
+        List<NamedGroup> namedGroups = Arrays.asList(NamedGroup.values());
+        tlsConfig.setDefaultClientNamedGroups(namedGroups);
+        List<SignatureAndHashAlgorithm> sigHashAlgos = Arrays.asList(SignatureAndHashAlgorithm.values());
+        tlsConfig.setDefaultClientSupportedSignatureAndHashAlgorithms(sigHashAlgos);
+        toTestList.remove(CipherSuite.TLS_FALLBACK_SCSV);
+        toTestList.remove(CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV);
+        tlsConfig.setDefaultClientSupportedCiphersuites(toTestList);
+        tlsConfig.setStopActionsAfterFatal(true);
+        return tlsConfig;
+    }
+
+    @Override
+    public boolean canBeExecuted(SiteReport report) {
+        return true;
+    }
+
+    @Override
+    public void adjustConfig(SiteReport report) {
+    }
+
+    @Override
+    public ProbeResult getCouldNotExecuteResult() {
+        return new CertificateResult(null, null);
+    }
+}
