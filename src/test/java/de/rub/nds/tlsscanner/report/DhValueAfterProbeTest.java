097d9873f84abbd71bd64f3a52d67d2b306bf2a2
hamedAsami <63098791+hamedAsami@users.noreply.github.com>
2020-04-21 12:21:41 +0200

    Update DhValueAfterProbeTest.java

--- src/test/java/de/rub/nds/tlsscanner/report/DhValueAfterProbeTest.java
+++ src/test/java/de/rub/nds/tlsscanner/report/DhValueAfterProbeTest.java
@@ -22,13 +22,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertEquals;
 
 /**
- *  Testset should check if  DhValueAfterProbe detects insecure DH-Parameters
- *  as insecure ones and secure parameters as secure ones
- *  
- *  Shortly: Checks If it correctly identifies the DH-Params
+ * Testset should check if DhValueAfterProbe detects insecure DH-Parameters as
+ * insecure ones and secure parameters as secure ones
  */
 
 public class DhValueAfterProbeTest {
@@ -51,21 +49,18 @@ public class DhValueAfterProbeTest {
     }
 
     /**
-     * test if method analyze of class DhValueAfterProbe 
-     * recognizes a secure public key as such
+     * Test if method analyze of class DhValueAfterProbe recognizes a secure
+     * public key as such
      */
     @Test
     public void secureDhParamTestAnalyze() {
         BigInteger securePubkey, secureMod;
         securePubkey = new BigInteger("65537");
         // openssl command for dhparams: openssl dhparam -text -noout 1024
-        secureMod = new BigInteger(
-            "00e8cc972fc56fe640588194e455522facbe4b09d88f5070e"
-            + "88f4b9b12acc85ee3d354f6fa85b81e46b557b0e0d75f71"
-            + "43b266a6346962fba3184bd30ca3a94bebb4b23ae269325"
-            + "dc15ac34b7bf38aa3dde5c6b2d9fe857237d3a7e5c7e9be"
-            + "938b187cd9781de993970e73a3fbf79a049a6d804a487de"
-            + "1013f71167cbf78aa65f3", 16);
+        secureMod = new BigInteger("00e8cc972fc56fe640588194e455522facbe4b09d88f5070e"
+                + "88f4b9b12acc85ee3d354f6fa85b81e46b557b0e0d75f71" + "43b266a6346962fba3184bd30ca3a94bebb4b23ae269325"
+                + "dc15ac34b7bf38aa3dde5c6b2d9fe857237d3a7e5c7e9be" + "938b187cd9781de993970e73a3fbf79a049a6d804a487de"
+                + "1013f71167cbf78aa65f3", 16);
 
         // store pubKey into list
         pubkeyContainer.put(securePubkey);
@@ -73,16 +68,14 @@ public class DhValueAfterProbeTest {
 
         analyseDhParams();
 
-        assertSame(test.getOnlyPrime(), TestResult.TRUE);
-        assertSame(test.getOnlySafePrime(), TestResult.TRUE);
-        assertSame(test.getUsesCommonDhPrimes(), TestResult.FALSE);
-        assertSame(test.getReuse(), TestResult.COULD_NOT_TEST);
+        assertEquals(test.getOnlyPrime(), TestResult.TRUE);
+        assertEquals(test.getOnlySafePrime(), TestResult.TRUE);
+        assertEquals(test.getUsesCommonDhPrimes(), TestResult.FALSE);
+        assertEquals(test.getReuse(), TestResult.COULD_NOT_TEST);
 
     }
 
-    /**
-     * Tests if method analyze recognizes an insecure parametes as such
-     */
+    /** Tests if method analyze recognizes an insecure parametes as such */
     @Test
     public void insecureDhParamTestAnalyze() {
         // examples for insecure values
@@ -94,29 +87,23 @@ public class DhValueAfterProbeTest {
 
         analyseDhParams();
 
-        assertSame(test.getOnlyPrime(), TestResult.FALSE);
-        assertSame(test.getOnlySafePrime(), TestResult.FALSE);
-        assertSame(test.getUsesCommonDhPrimes(), TestResult.FALSE);
-        assertSame(test.getReuse(), TestResult.COULD_NOT_TEST);
+        assertEquals(test.getOnlyPrime(), TestResult.FALSE);
+        assertEquals(test.getOnlySafePrime(), TestResult.FALSE);
+        assertEquals(test.getUsesCommonDhPrimes(), TestResult.FALSE);
+        assertEquals(test.getReuse(), TestResult.COULD_NOT_TEST);
 
     }
 
-    /**
-     * Test if method anaylize detects reused public
-     * key
-     */
+    /** Test if method anaylize detects reused publickey */
     @Test
     public void secureReusedDhPubkeyTestAnalyze() {
         BigInteger secureKey, secureMod;
 
         secureKey = new BigInteger("65537");
-        secureMod = new BigInteger(
-            "00e8a678364bb6f7d85d4b29ecfedab6d6caa88eb90c4ca1"
-            + "5a43a3542cdd5c39ef42bbde1b4b9b5715ae14bdedd78d" 
-            + "6b5262f5ac9c2fdec09a612ef3aea969ce1327a6b5c9f3"
-            + "ac052faafebbabc9c9679bd14e0a26114ff032c95d2ed7"
-            + "3ed60cd64f497094bd4cb5839f9d7ad58fd4ccac343db5"
-            + "81c4bf8032259bc1a3d7ee4d03", 16);
+        secureMod = new BigInteger("00e8a678364bb6f7d85d4b29ecfedab6d6caa88eb90c4ca1"
+                + "5a43a3542cdd5c39ef42bbde1b4b9b5715ae14bdedd78d" + "6b5262f5ac9c2fdec09a612ef3aea969ce1327a6b5c9f3"
+                + "ac052faafebbabc9c9679bd14e0a26114ff032c95d2ed7" + "3ed60cd64f497094bd4cb5839f9d7ad58fd4ccac343db5"
+                + "81c4bf8032259bc1a3d7ee4d03", 16);
 
         // reuse pubKey
         pubkeyContainer.put(secureKey);
@@ -126,10 +113,10 @@ public class DhValueAfterProbeTest {
 
         analyseDhParams();
 
-        assertSame(test.getOnlyPrime(), TestResult.TRUE);
-        assertSame(test.getOnlySafePrime(), TestResult.TRUE);
-        assertSame(test.getUsesCommonDhPrimes(), TestResult.FALSE);
-        assertSame(test.getReuse(), TestResult.TRUE);
+        assertEquals(test.getOnlyPrime(), TestResult.TRUE);
+        assertEquals(test.getOnlySafePrime(), TestResult.TRUE);
+        assertEquals(test.getUsesCommonDhPrimes(), TestResult.FALSE);
+        assertEquals(test.getReuse(), TestResult.TRUE);
 
     }
 
