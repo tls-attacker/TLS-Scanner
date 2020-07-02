a4d465520ecc9745a0eab752fd2e0661d2c75461
dziebart <denni.z@gmx.net>
2020-07-02 20:30:57 +0200

    Added seperate Method for TLS 1.3 Random Collection

--- src/main/java/de/rub/nds/tlsscanner/probe/TlsRngProbe.java
+++ src/main/java/de/rub/nds/tlsscanner/probe/TlsRngProbe.java
@@ -8,6 +8,7 @@
  */
 package de.rub.nds.tlsscanner.probe;
 
+import de.rub.nds.modifiablevariable.util.ArrayConverter;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.*;
 import de.rub.nds.tlsattacker.core.https.HttpsRequestMessage;
@@ -18,6 +19,7 @@ import de.rub.nds.tlsattacker.core.protocol.message.*;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.action.*;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.constants.ProbeType;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
 import de.rub.nds.tlsscanner.config.ScannerConfig;
@@ -55,21 +57,24 @@ public class TlsRngProbe extends TlsProbe {
         // activate
         if (latestReport.getResult(AnalyzedProperty.SUPPORTS_TLS_1_3) == TestResult.TRUE) {
             LOGGER.warn("SETTING HIGHEST VERSION TO TLS13");
+            collectServerRandomTls13(4, 1);
             highestVersion = ProtocolVersion.TLS13;
         } else if (latestReport.getResult(AnalyzedProperty.SUPPORTS_TLS_1_2) == TestResult.TRUE) {
             LOGGER.warn("SETTING HIGHEST VERSION TO TLS12");
+            collectServerRandom(4, 1);
             highestVersion = ProtocolVersion.TLS13;
         } else if (latestReport.getResult(AnalyzedProperty.SUPPORTS_TLS_1_1) == TestResult.TRUE) {
             LOGGER.warn("SETTING HIGHEST VERSION TO TLS11");
+            collectServerRandom(4, 1);
             highestVersion = ProtocolVersion.TLS11;
         } else if (latestReport.getResult(AnalyzedProperty.SUPPORTS_TLS_1_0) == TestResult.TRUE) {
             LOGGER.warn("SETTING HIGHEST VERSION TO TLS10");
+            collectServerRandom(4, 1);
             highestVersion = ProtocolVersion.TLS10;
         }
 
         // ////////////////////////////////////////////////////////////////////////////////////////////////////
-        collectServerRandom(4,1);
-        collectIV(100,10);
+        collectIV(100, 10);
         // /////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // TODO: Implement this right.
@@ -113,19 +118,22 @@ public class TlsRngProbe extends TlsProbe {
     private Config generateTestConfig(byte[] clientRandom) {
         Config testConf = getScannerConfig().createConfig();
 
-        List<CipherSuite> fallBackECDHCipherSuites = new LinkedList<>();
-        for (CipherSuite cipherSuite : CipherSuite.values()) {
-            if (cipherSuite.name().contains("TLS_ECDHE") || cipherSuite.name().contains("TLS_DHE")) {
-                fallBackECDHCipherSuites.add(cipherSuite);
-            }
-        }
+        CipherSuite[] fallBackCipherSuites = new CipherSuite[latestReport.getCipherSuites().toArray().length];
+        fallBackCipherSuites = latestReport.getCipherSuites().toArray(fallBackCipherSuites);
+        // for (CipherSuite cipherSuite : CipherSuite.values()) {
+        // if (cipherSuite.name().contains("TLS_ECDHE") ||
+        // cipherSuite.name().contains("TLS_DHE")) {
+        // fallBackCipherSuites.add(cipherSuite);
+        // }
+        // }
 
-        List<NamedGroup> groups = new LinkedList<>();
-        groups.addAll(Arrays.asList(NamedGroup.values()));
+        // List<NamedGroup> groups = new LinkedList<>();
+        // groups.addAll(Arrays.asList(NamedGroup.values()));
+        // LOGGER.warn(groups.toString());
 
-        testConf.setDefaultClientSupportedCiphersuites(fallBackECDHCipherSuites);
+        testConf.setDefaultClientSupportedCiphersuites(fallBackCipherSuites);
         testConf.setEnforceSettings(false);
-        testConf.setAddServerNameIndicationExtension(true);
+        // testConf.setAddServerNameIndicationExtension(true);
         testConf.setAddEllipticCurveExtension(true);
         testConf.setAddECPointFormatExtension(true);
         testConf.setAddSignatureAndHashAlgorithmsExtension(true);
@@ -139,12 +147,114 @@ public class TlsRngProbe extends TlsProbe {
         testConf.setEarlyStop(false);
 
         testConf.setStopActionsAfterFatal(true);
-        testConf.setDefaultClientNamedGroups(groups);
+        // testConf.setDefaultClientNamedGroups(groups);
+        testConf.setDefaultClientNamedGroups(latestReport.getSupportedNamedGroups());
 
         return testConf;
     }
 
-    private void collectServerRandom(int numberOfHandshakes, int clientRandomInit){
+    private Config generateTls13Config(byte[] clientRandom) {
+        Config tlsConfig = getScannerConfig().createConfig();
+        tlsConfig.setQuickReceive(true);
+        tlsConfig.setHighestProtocolVersion(ProtocolVersion.TLS13);
+        tlsConfig.setSupportedVersions(ProtocolVersion.TLS13);
+        tlsConfig.setEnforceSettings(false);
+        tlsConfig.setEarlyStop(true);
+        tlsConfig.setStopReceivingAfterFatal(true);
+        tlsConfig.setStopActionsAfterFatal(true);
+        List<NamedGroup> tls13Groups = new LinkedList<>();
+        for (NamedGroup group : NamedGroup.values()) {
+            if (group.isTls13() && !(group.name().contains("FFDHE"))) {
+                tls13Groups.add(group);
+            }
+        }
+        tlsConfig.setDefaultClientNamedGroups(tls13Groups);
+        tlsConfig.setAddECPointFormatExtension(false);
+        tlsConfig.setAddEllipticCurveExtension(true);
+        tlsConfig.setAddSignatureAndHashAlgorithmsExtension(true);
+        tlsConfig.setAddSupportedVersionsExtension(true);
+        tlsConfig.setAddKeyShareExtension(true);
+        tlsConfig.setAddServerNameIndicationExtension(true);
+        tlsConfig.setUseFreshRandom(false);
+        tlsConfig.setDefaultClientRandom(clientRandom);
+
+        List<SignatureAndHashAlgorithm> algos = new LinkedList<>();
+        algos.add(SignatureAndHashAlgorithm.RSA_SHA256);
+        algos.add(SignatureAndHashAlgorithm.RSA_SHA384);
+        algos.add(SignatureAndHashAlgorithm.RSA_SHA512);
+        algos.add(SignatureAndHashAlgorithm.ECDSA_SHA256);
+        algos.add(SignatureAndHashAlgorithm.ECDSA_SHA384);
+        algos.add(SignatureAndHashAlgorithm.ECDSA_SHA512);
+        algos.add(SignatureAndHashAlgorithm.RSA_PSS_PSS_SHA256);
+        algos.add(SignatureAndHashAlgorithm.RSA_PSS_PSS_SHA384);
+        algos.add(SignatureAndHashAlgorithm.RSA_PSS_PSS_SHA512);
+        algos.add(SignatureAndHashAlgorithm.RSA_PSS_RSAE_SHA256);
+        algos.add(SignatureAndHashAlgorithm.RSA_PSS_RSAE_SHA384);
+        algos.add(SignatureAndHashAlgorithm.RSA_PSS_RSAE_SHA512);
+
+        tlsConfig.setDefaultClientSupportedSignatureAndHashAlgorithms(algos);
+        return tlsConfig;
+    }
+
+    private void collectServerRandomTls13(int numberOfHandshakes, int clientRandomInit) {
+        List<CipherSuite> serverHelloCollectSuites = new LinkedList<>();
+        CipherSuite[] supportedSuites = new CipherSuite[latestReport.getSupportedTls13CipherSuites().toArray().length];
+        supportedSuites = latestReport.getSupportedTls13CipherSuites().toArray(supportedSuites);
+        if (latestReport.getResult(AnalyzedProperty.SUPPORTS_RSA) == TestResult.TRUE) {
+            for (CipherSuite cipherSuite : supportedSuites) {
+                if (cipherSuite.name().contains("TLS_RSA")) {
+                    serverHelloCollectSuites.add(cipherSuite);
+                }
+            }
+        } else if (latestReport.getResult(AnalyzedProperty.SUPPORTS_DH) == TestResult.TRUE) {
+            for (CipherSuite cipherSuite : supportedSuites) {
+                if (cipherSuite.name().contains("TLS_DH")) {
+                    serverHelloCollectSuites.add(cipherSuite);
+                }
+            }
+        } else if (latestReport.getResult(AnalyzedProperty.SUPPORTS_STATIC_ECDH) == TestResult.TRUE) {
+            for (CipherSuite cipherSuite : supportedSuites) {
+                if (cipherSuite.name().contains("TLS_ECDH")) {
+                    serverHelloCollectSuites.add(cipherSuite);
+                }
+            }
+        }
+
+        boolean supportsExtendedRandom = latestReport.getSupportedExtensions().contains(ExtensionType.EXTENDED_RANDOM);
+
+        for (int i = 0; i < numberOfHandshakes; i++) {
+            Config serverHelloConfig = generateTls13Config(intToByteArray(clientRandomInit + i));
+
+            if (supportsExtendedRandom) {
+                LOGGER.warn("Extended Random Supported!");
+                serverHelloConfig.setParseKeyShareOld(false);
+                serverHelloConfig.setAddExtendedRandomExtension(true);
+            }
+
+            if (!serverHelloCollectSuites.isEmpty()) {
+                serverHelloConfig.setDefaultClientSupportedCiphersuites(serverHelloCollectSuites);
+            } else {
+                // Fallback to supported Suites
+                serverHelloConfig.setDefaultClientSupportedCiphersuites(supportedSuites);
+            }
+            serverHelloConfig.setEnforceSettings(true);
+
+            serverHelloConfig.setWorkflowTraceType(WorkflowTraceType.SHORT_HELLO);
+
+            State test_state = new State(serverHelloConfig);
+            LOGGER.warn("Starting test ClientHello in TLS 13");
+            executeState(test_state);
+            LOGGER.warn("===========================================================================================");
+            LOGGER.warn(test_state.getWorkflowTrace());
+            LOGGER.warn(test_state.getTlsContext().getSelectedProtocolVersion());
+            LOGGER.warn(test_state.getTlsContext().getSelectedCipherSuite());
+            LOGGER.warn(ArrayConverter.bytesToHexString(test_state.getTlsContext().getServerRandom()));
+            LOGGER.warn("===========================================================================================");
+        }
+
+    }
+
+    private void collectServerRandom(int numberOfHandshakes, int clientRandomInit) {
         // Use preferred Ciphersuites if supported
         List<CipherSuite> serverHelloCollectSuites = new LinkedList<>();
         CipherSuite[] supportedSuites = new CipherSuite[latestReport.getCipherSuites().toArray().length];
@@ -168,40 +278,44 @@ public class TlsRngProbe extends TlsProbe {
                 }
             }
         }
-        // If not one of the preferred Cipher suites is supported, use standard
-        // cipher suites configured
-        // in generateConfig method (i.e. ECDHE cipher suites )
 
         boolean supportsExtendedRandom = latestReport.getSupportedExtensions().contains(ExtensionType.EXTENDED_RANDOM);
 
         for (int i = 0; i < numberOfHandshakes; i++) {
             WorkflowTrace randomnessTest = new WorkflowTrace();
-            Config serverHelloConfig = generateTestConfig(intToByteArray(clientRandomInit+i));
+            Config serverHelloConfig = generateTestConfig(intToByteArray(clientRandomInit + i));
+
             if (supportsExtendedRandom) {
                 LOGGER.warn("Extended Random Supported!");
                 serverHelloConfig.setParseKeyShareOld(false);
                 serverHelloConfig.setAddExtendedRandomExtension(true);
             }
             serverHelloConfig.setHighestProtocolVersion(highestVersion);
+            serverHelloConfig.setSupportedVersions(highestVersion);
             if (!serverHelloCollectSuites.isEmpty()) {
                 serverHelloConfig.setDefaultClientSupportedCiphersuites(serverHelloCollectSuites);
+            } else {
+                // Fallback to supported Suites
+                serverHelloConfig.setDefaultClientSupportedCiphersuites(supportedSuites);
             }
+            serverHelloConfig.setEnforceSettings(true);
+
             ClientHelloMessage client_test = new ClientHelloMessage(serverHelloConfig);
             randomnessTest.addTlsActions(new SendAction(client_test));
             randomnessTest.addTlsActions(new ReceiveAction(new ServerHelloMessage(serverHelloConfig)));
             State test_state = new State(serverHelloConfig, randomnessTest);
-
             LOGGER.warn("Starting test ClientHello");
             executeState(test_state);
             LOGGER.warn("===========================================================================================");
             LOGGER.warn(test_state.getWorkflowTrace());
             LOGGER.warn(test_state.getTlsContext().getSelectedProtocolVersion());
             LOGGER.warn(test_state.getTlsContext().getSelectedCipherSuite());
+            LOGGER.warn(ArrayConverter.bytesToHexString(test_state.getTlsContext().getServerRandom()));
             LOGGER.warn("===========================================================================================");
         }
     }
 
-    private void collectIV(int numberOfBlocks, int clientRandomInit){
+    private void collectIV(int numberOfBlocks, int clientRandomInit) {
         // Collect IV
         // Here it is not important which ciphersuite we use for key-exchange,
         // only important thing is maximum
@@ -217,9 +331,11 @@ public class TlsRngProbe extends TlsProbe {
                     cbcSuites.add(suite);
                 }
             }
-            if(cbcSuites.isEmpty()){
-                // No Cipher suites allow CBC Mode --> Collect more ServerRandoms instead
+            if (cbcSuites.isEmpty()) {
+                // No Cipher suites allow CBC Mode --> Collect more
+                // ServerRandoms instead
                 // TODO: Implement Fallback to collect Server Randoms
+                return;
             } else {
                 iVCollectConfig.setDefaultClientSupportedCiphersuites(cbcSuites);
             }
