01c68283afcfa6d208fc90a6734b5e33e3888e7a
Nurullah Erinola <n.erinola@gmx.de>
2020-12-02 14:57:12 +0100

    Adapted for dtls

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/TlsPoodleProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/TlsPoodleProbe.java
@@ -11,12 +11,13 @@ package de.rub.nds.tlsscanner.serverscanner.probe;
 import de.rub.nds.tlsattacker.attacks.config.TLSPoodleCommandConfig;
 import de.rub.nds.tlsattacker.attacks.impl.TLSPoodleAttacker;
 import de.rub.nds.tlsattacker.core.config.delegate.ClientDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.ProtocolVersionDelegate;
 import de.rub.nds.tlsattacker.core.config.delegate.StarttlsDelegate;
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
 import de.rub.nds.tlsscanner.serverscanner.config.ScannerConfig;
 import de.rub.nds.tlsscanner.serverscanner.constants.ProbeType;
 import de.rub.nds.tlsscanner.serverscanner.rating.TestResult;
-import de.rub.nds.tlsscanner.serverscanner.report.AnalyzedProperty;
 import de.rub.nds.tlsscanner.serverscanner.report.SiteReport;
 import de.rub.nds.tlsscanner.serverscanner.report.result.ProbeResult;
 import de.rub.nds.tlsscanner.serverscanner.report.result.TlsPoodleResult;
@@ -39,6 +40,11 @@ public class TlsPoodleProbe extends TlsProbe {
             ClientDelegate delegate = (ClientDelegate) poodleCommandConfig.getDelegate(ClientDelegate.class);
             delegate.setHost(getScannerConfig().getClientDelegate().getHost());
             delegate.setSniHostname(getScannerConfig().getClientDelegate().getSniHostname());
+            if (getScannerConfig().getDtlsDelegate().isDTLS()) {
+                ProtocolVersionDelegate protocolVersionDelegate = (ProtocolVersionDelegate) poodleCommandConfig
+                        .getDelegate(ProtocolVersionDelegate.class);
+                protocolVersionDelegate.setProtocolVersion(ProtocolVersion.DTLS12);
+            }
             StarttlsDelegate starttlsDelegate = (StarttlsDelegate) poodleCommandConfig
                     .getDelegate(StarttlsDelegate.class);
             starttlsDelegate.setStarttlsType(scannerConfig.getStarttlsDelegate().getStarttlsType());
@@ -53,7 +59,7 @@ public class TlsPoodleProbe extends TlsProbe {
 
     @Override
     public boolean canBeExecuted(SiteReport report) {
-        return report.getResult(AnalyzedProperty.SUPPORTS_BLOCK_CIPHERS) == TestResult.TRUE;
+        return true;
     }
 
     @Override
