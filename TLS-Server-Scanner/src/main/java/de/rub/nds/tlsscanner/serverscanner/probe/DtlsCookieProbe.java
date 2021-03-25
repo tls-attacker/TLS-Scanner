487683a3ae65efede29b9459540083942453cb68
Nurullah Erinola <n.erinola@gmx.de>
2021-03-25 17:38:27 +0100

    Changed GenericReceiveActions to ReceiveTillActions

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/DtlsCookieProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/DtlsCookieProbe.java
@@ -18,12 +18,13 @@ import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.HelloVerifyRequestMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ServerHelloDoneMessage;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
-import de.rub.nds.tlsattacker.core.workflow.action.GenericReceiveAction;
 import de.rub.nds.tlsattacker.core.workflow.action.ReceiveAction;
+import de.rub.nds.tlsattacker.core.workflow.action.ReceiveTillAction;
 import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsscanner.serverscanner.config.ScannerConfig;
@@ -77,7 +78,7 @@ public class DtlsCookieProbe extends TlsProbe {
             cookie.setModification(ByteArrayModificationFactory.xor(ArrayConverter.hexStringToByteArray("FF"), totest));
             clientHelloMessage.setCookie(cookie);
             trace.addTlsAction(new SendAction(clientHelloMessage));
-            trace.addTlsAction(new GenericReceiveAction());
+            trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage(config)));
             State state = new State(config, trace);
             if (getResult(state) == TestResult.FALSE) {
                 return TestResult.FALSE;
@@ -97,7 +98,7 @@ public class DtlsCookieProbe extends TlsProbe {
         protocolVersion.setModification(ByteArrayModificationFactory.explicitValue(ProtocolVersion.DTLS10.getValue()));
         clientHelloMessage.setProtocolVersion(protocolVersion);
         trace.addTlsAction(new SendAction(clientHelloMessage));
-        trace.addTlsAction(new GenericReceiveAction());
+        trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage(config)));
         State state = new State(config, trace);
         return getResult(state);
     }
@@ -113,7 +114,7 @@ public class DtlsCookieProbe extends TlsProbe {
         random.setModification(ByteArrayModificationFactory.xor(ArrayConverter.hexStringToByteArray("FFFF"), -2));
         clientHelloMessage.setRandom(random);
         trace.addTlsAction(new SendAction(clientHelloMessage));
-        trace.addTlsAction(new GenericReceiveAction());
+        trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage(config)));
         State state = new State(config, trace);
         return getResult(state);
     }
@@ -129,7 +130,7 @@ public class DtlsCookieProbe extends TlsProbe {
         ciphersuites.setModification(ByteArrayModificationFactory.delete(1, 2));
         clientHelloMessage.setCipherSuites(ciphersuites);
         trace.addTlsAction(new SendAction(clientHelloMessage));
-        trace.addTlsAction(new GenericReceiveAction());
+        trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage(config)));
         State state = new State(config, trace);
         return getResult(state);
     }
@@ -145,7 +146,7 @@ public class DtlsCookieProbe extends TlsProbe {
         compressions.setModification(ByteArrayModificationFactory.delete(-1, 1));
         clientHelloMessage.setCompressions(compressions);
         trace.addTlsAction(new SendAction(clientHelloMessage));
-        trace.addTlsAction(new GenericReceiveAction());
+        trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage(config)));
         State state = new State(config, trace);
         return getResult(state);
     }
