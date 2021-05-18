c0505cbc1bbcafc5fe40b97be6c2831e1f870b0b
Nurullah Erinola <n.erinola@gmx.de>
2021-05-18 21:35:44 +0200

    Adapted config for dtls

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/CcaSupportProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/CcaSupportProbe.java
@@ -10,24 +10,23 @@
 package de.rub.nds.tlsscanner.serverscanner.probe;
 
 import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.constants.CompressionMethod;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
-import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
-import de.rub.nds.tlsattacker.core.protocol.message.ServerHelloDoneMessage;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
-import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
-import de.rub.nds.tlsattacker.core.workflow.action.ReceiveTillAction;
-import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
-import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
-import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.serverscanner.config.ScannerConfig;
 import de.rub.nds.tlsscanner.serverscanner.constants.ProbeType;
 import de.rub.nds.tlsscanner.serverscanner.rating.TestResult;
 import de.rub.nds.tlsscanner.serverscanner.report.SiteReport;
 import de.rub.nds.tlsscanner.serverscanner.report.result.CcaSupportResult;
 import de.rub.nds.tlsscanner.serverscanner.report.result.ProbeResult;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CcaSupportProbe extends TlsProbe {
 
@@ -38,12 +37,7 @@ public class CcaSupportProbe extends TlsProbe {
     @Override
     public ProbeResult executeTest() {
         Config tlsConfig = generateConfig();
-
-        WorkflowTrace trace = new WorkflowConfigurationFactory(tlsConfig)
-            .createTlsEntryWorkflowTrace(tlsConfig.getDefaultClientConnection());
-        trace.addTlsAction(new SendAction(new ClientHelloMessage(tlsConfig)));
-        trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage()));
-        State state = new State(tlsConfig, trace);
+        State state = new State(tlsConfig);
         try {
             executeState(state);
         } catch (Exception e) {
@@ -73,14 +67,27 @@ public class CcaSupportProbe extends TlsProbe {
     private Config generateConfig() {
         Config config = getScannerConfig().createConfig();
         config.setAutoSelectCertificate(false);
-        config.setWorkflowTraceType(WorkflowTraceType.HELLO);
-        config.setDefaultSelectedProtocolVersion(ProtocolVersion.TLS10);
-
+        if (getScannerConfig().getDtlsDelegate().isDTLS()) {
+            config.setHighestProtocolVersion(ProtocolVersion.DTLS12);
+        } else {
+            config.setHighestProtocolVersion(ProtocolVersion.TLS12);
+        }
+        List<CipherSuite> ciphersuites = new LinkedList<>();
+        ciphersuites.addAll(Arrays.asList(CipherSuite.values()));
+        ciphersuites.remove(CipherSuite.TLS_FALLBACK_SCSV);
+        ciphersuites.remove(CipherSuite.TLS_EMPTY_RENEGOTIATION_INFO_SCSV);
+        config.setDefaultClientSupportedCipherSuites(ciphersuites);
+        List<CompressionMethod> compressionList = new ArrayList<>(Arrays.asList(CompressionMethod.values()));
+        config.setDefaultClientSupportedCompressionMethods(compressionList);
+        config.setEnforceSettings(false);
         config.setQuickReceive(true);
         config.setEarlyStop(true);
-        config.setStopActionsAfterIOException(true);
+        config.setStopReceivingAfterFatal(true);
         config.setStopActionsAfterFatal(true);
-
+        config.setStopActionsAfterIOException(true);
+        config.setAddECPointFormatExtension(true);
+        config.setAddEllipticCurveExtension(true);
+        config.setAddSignatureAndHashAlgorithmsExtension(true);
         return config;
     }
 }
