ce3b09a8a587ba03c1e6697a9634cf6f4f70d5a1
Sven Niclas Hebrok <snhebrok@mail.uni-paderborn.de>
2021-07-30 11:58:55 +0200

    Switch to nip.io as xip.io is down

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/config/OrchestratorDelegate.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/config/OrchestratorDelegate.java
@@ -8,8 +8,8 @@ import de.rub.nds.tlsattacker.core.exceptions.ConfigurationException;
 import de.rub.nds.tlsscanner.clientscanner.util.IPUtil;
 
 public class OrchestratorDelegate extends Delegate {
-    @Parameter(names = "-serverBaseURL", required = false, description = "Base URL to use for the server. Defaults to 127.0.0.1.xip.io. Can be set to an IPv4, in this case -noSubdomain is implied")
-    protected String serverBaseURL = "127.0.0.1.xip.io";
+    @Parameter(names = "-serverBaseURL", required = false, description = "Base URL to use for the server. Defaults to 127.0.0.1.nip.io. Can be set to an IPv4, in this case -noSubdomain is implied")
+    protected String serverBaseURL = "127.0.0.1.nip.io";
     @Parameter(names = "-noEntryDispatcher", description = "Do not add ChloEntryDispatcher, this allows probes to modify how the entry trace is created.")
     protected boolean noEntryDispatcher = false;
     @Parameter(names = "-singleDomain", description = "Use single domain instead of using unique subdomains for each probe. Note that this will most likely cause threading to be less effective")
@@ -19,6 +19,7 @@ public class OrchestratorDelegate extends Delegate {
     public void applyDelegate(Config config) throws ConfigurationException {
         // Nothing to do
     }
+
     public String getServerBaseURL() {
         return serverBaseURL;
     }
