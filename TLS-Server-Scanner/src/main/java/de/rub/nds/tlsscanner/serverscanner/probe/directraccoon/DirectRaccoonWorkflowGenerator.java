9d4fffa7fae07d2e8b3f3790c47a8e2b594191f0
Nurullah Erinola <n.erinola@gmx.de>
2021-11-08 10:45:40 +0100

    Simplified workflow trace

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/directraccoon/DirectRaccoonWorkflowGenerator.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/directraccoon/DirectRaccoonWorkflowGenerator.java
@@ -10,18 +10,15 @@
 package de.rub.nds.tlsscanner.serverscanner.probe.directraccoon;
 
 import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.RunningModeType;
 import de.rub.nds.tlsattacker.core.protocol.message.ChangeCipherSpecMessage;
-import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.FinishedMessage;
-import de.rub.nds.tlsattacker.core.protocol.message.HelloVerifyRequestMessage;
-import de.rub.nds.tlsattacker.core.protocol.message.ServerHelloDoneMessage;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.action.GenericReceiveAction;
-import de.rub.nds.tlsattacker.core.workflow.action.ReceiveAction;
-import de.rub.nds.tlsattacker.core.workflow.action.ReceiveTillAction;
 import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
 import de.rub.nds.tlsattacker.core.workflow.action.SendRaccoonCkeAction;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import java.math.BigInteger;
 
 /**
@@ -33,13 +30,7 @@ public class DirectRaccoonWorkflowGenerator {
     public static WorkflowTrace generateWorkflow(Config tlsConfig, DirectRaccoonWorkflowType type,
         BigInteger initialDhSecret, boolean withNullByte) {
         WorkflowTrace trace = new WorkflowConfigurationFactory(tlsConfig)
-            .createTlsEntryWorkflowTrace(tlsConfig.getDefaultClientConnection());
-        trace.addTlsAction(new SendAction(new ClientHelloMessage(tlsConfig)));
-        if (tlsConfig.getHighestProtocolVersion().isDTLS()) {
-            trace.addTlsAction(new ReceiveAction(new HelloVerifyRequestMessage(tlsConfig)));
-            trace.addTlsAction(new SendAction(new ClientHelloMessage(tlsConfig)));
-        }
-        trace.addTlsAction(new ReceiveTillAction(new ServerHelloDoneMessage(tlsConfig)));
+            .createWorkflowTrace(WorkflowTraceType.DYNAMIC_HELLO, RunningModeType.CLIENT);
         trace.addTlsAction(new SendRaccoonCkeAction(withNullByte, initialDhSecret));
         if (null != type) {
             switch (type) {
