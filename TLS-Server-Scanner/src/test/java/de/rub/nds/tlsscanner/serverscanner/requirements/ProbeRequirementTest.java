fe6c5b0589337df78defc43e0b1edbce87506145
Jia-Lin Shi <j_shi003ni-muenster.de>
2022-03-16 15:01:11 +0100

    added placeholder test functions

---
+++ TLS-Server-Scanner/src/test/java/de/rub/nds/tlsscanner/serverscanner/requirements/ProbeRequirementTest.java
@@ -0,0 +1,31 @@
+package de.rub.nds.tlsscanner.serverscanner.requirements;
+
+import org.junit.*;
+
+public class ProbeRequirementTest {
+	
+	/**
+	 * Set up of testing
+	 */
+	@Before
+	public void setUp() {
+		
+	}
+	
+	/**
+	 * Test different instantiations of the probe requirements
+	 */
+	@Test
+	public void createProbeRequirementsTest() {
+		
+	}
+	
+
+	/**
+	 * Test evaluation of the probe requirements
+	 */
+	@Test
+	public void evaluateProbeRequirementsTest() {
+		
+	}
+}
