8161370b3ec374e51602fe62264d955a9216551a
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-03-25 22:38:57 +0100

    added properties to probe cert

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/CertificateProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/CertificateProbe.java
@@ -57,6 +57,11 @@ public class CertificateProbe extends TlsProbe {
 
     public CertificateProbe(ScannerConfig config, ParallelExecutor parallelExecutor) {
         super(parallelExecutor, ProbeType.CERTIFICATE, config);
+        properties.add(AnalyzedProperty.SUPPORTS_RSA_CERT);
+        properties.add(AnalyzedProperty.SUPPORTS_ECDSA);
+        properties.add(AnalyzedProperty.SUPPORTS_DSS);
+        properties.add(AnalyzedProperty.SUPPORTS_GOST);
+        properties.add(AnalyzedProperty.SUPPORTS_TLS_1_3);
     }
 
     @Override
