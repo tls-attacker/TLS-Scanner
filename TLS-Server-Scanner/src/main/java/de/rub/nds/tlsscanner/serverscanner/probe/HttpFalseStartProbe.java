4006e73a45d4e3a80abdef01828fc1a1953da5c0
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-03-28 16:49:54 +0200

    added properties to probe httpfalsestart

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/HttpFalseStartProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/HttpFalseStartProbe.java
@@ -45,6 +45,7 @@ public class HttpFalseStartProbe extends HttpsProbe {
 
     public HttpFalseStartProbe(ScannerConfig scannerConfig, ParallelExecutor parallelExecutor) {
         super(parallelExecutor, ProbeType.HTTP_FALSE_START, scannerConfig);
+        properties.add(AnalyzedProperty.SUPPORTS_HTTP_FALSE_START);
     }
 
     @Override
