422738a100c482f5c93db89d0bdd265fa908f999
ic0ns <robert.merget@rub.de>
2022-03-29 12:11:17 +0200

    removed additional license header

--- Scanner-Core/src/main/java/de/rub/nds/scanner/core/util/ConsoleLogger.java
+++ Scanner-Core/src/main/java/de/rub/nds/scanner/core/util/ConsoleLogger.java
@@ -9,15 +9,6 @@
 
 package de.rub.nds.scanner.core.util;
 
-/**
- * TLS-Server-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
- *
- * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
- *
- * Licensed under Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0.txt
- */
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
