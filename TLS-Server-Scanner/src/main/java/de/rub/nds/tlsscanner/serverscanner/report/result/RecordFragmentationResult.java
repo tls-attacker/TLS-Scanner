588a93d5c1adb4f907b91792688c7f6bfe808d5a
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-03-31 22:38:23 +0200

    updated merge data of probe record fragmentation

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/report/result/RecordFragmentationResult.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/report/result/RecordFragmentationResult.java
@@ -10,7 +10,6 @@
 package de.rub.nds.tlsscanner.serverscanner.report.result;
 
 import de.rub.nds.tlsscanner.serverscanner.constants.ProbeType;
-import de.rub.nds.tlsscanner.serverscanner.rating.TestResult;
 import de.rub.nds.tlsscanner.serverscanner.report.AnalyzedProperty;
 import de.rub.nds.tlsscanner.serverscanner.report.SiteReport;
 
