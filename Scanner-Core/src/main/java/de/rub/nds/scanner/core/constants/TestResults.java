0812a2fb837f6b5bbabfbacd1b7dc5f45a51571c
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-04-06 17:29:05 +0200

    added TestResults in core

---
+++ Scanner-Core/src/main/java/de/rub/nds/scanner/core/constants/TestResults.java
@@ -0,0 +1,36 @@
+/**
+ * Scanner-Core - A TLS configuration and analysis tool based on TLS-Attacker
+ *
+ * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package de.rub.nds.scanner.core.constants;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="result")
+@XmlAccessorType(XmlAccessType.FIELD)
+public enum TestResults implements TestResult {
+    TRUE,
+    FALSE,
+    PARTIALLY,
+    CANNOT_BE_TESTED,
+    COULD_NOT_TEST,
+    ERROR_DURING_TEST,
+    UNCERTAIN,
+    UNSUPPORTED,
+    NOT_TESTED_YET,
+    TIMEOUT;
+
+    private TestResults() {
+    }
+
+    public static TestResults of(boolean value) {
+        return value ? TRUE : FALSE;
+    }
+}
