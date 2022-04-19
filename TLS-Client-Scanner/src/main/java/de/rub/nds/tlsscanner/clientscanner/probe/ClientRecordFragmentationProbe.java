68f5822eacb6ff74e2bb6905981f57693261bd21
Avent22 <Nils.Kluemper@ruhr-uni-bochum.de>
2022-04-19 20:17:27 +0200

    Changing the workflowtrace (adding the finished message), changing the requirement for the test to an received finished message

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ClientRecordFragmentationProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ClientRecordFragmentationProbe.java
@@ -1,27 +1,24 @@
-/*
- * Copyright 2022 nk.
+/**
+ * TLS-Client-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
+
 package de.rub.nds.tlsscanner.clientscanner.probe;
 
 import de.rub.nds.tlsscanner.core.probe.TlsProbe;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.RunningModeType;
+import de.rub.nds.tlsattacker.core.protocol.message.FinishedMessage;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
+import de.rub.nds.tlsattacker.core.workflow.action.ReceiveTillAction;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
@@ -29,12 +26,10 @@ import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
 
 import de.rub.nds.tlsscanner.clientscanner.probe.result.ClientRecordFragmentationResult;
-/**
- *
- * 
- */
-public class ClientRecordFragmentationProbe extends TlsProbe<ClientScannerConfig, ClientReport, ClientRecordFragmentationResult>{
-    
+
+public class ClientRecordFragmentationProbe
+    extends TlsProbe<ClientScannerConfig, ClientReport, ClientRecordFragmentationResult> {
+
     public ClientRecordFragmentationProbe(ClientScannerConfig scannerConfig, ParallelExecutor parallelExecutor) {
         super(parallelExecutor, TlsProbeType.RECORD_FRAGMENTATION, scannerConfig);
     }
@@ -43,14 +38,16 @@ public class ClientRecordFragmentationProbe extends TlsProbe<ClientScannerConfig
     public ClientRecordFragmentationResult executeTest() {
         Config config = getScannerConfig().createConfig();
         config.setDefaultMaxRecordData(50);
-
-        State state = new State(config, new WorkflowConfigurationFactory(config)
-            .createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER));
+        
+        WorkflowConfigurationFactory factory = new WorkflowConfigurationFactory(config);
+        WorkflowTrace workflowTrace = factory.createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
+        workflowTrace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+        State state = new State(config, workflowTrace);
 
         executeState(state);
 
         return new ClientRecordFragmentationResult(
-            WorkflowTraceUtil.didSendMessage(HandshakeMessageType.SERVER_HELLO_DONE, state.getWorkflowTrace()));
+            WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, state.getWorkflowTrace()));
     }
 
     @Override
@@ -67,5 +64,5 @@ public class ClientRecordFragmentationProbe extends TlsProbe<ClientScannerConfig
     public void adjustConfig(ClientReport report) {
 
     }
-    
+
 }
