6f34ffa5ed7f449bc3fdd87c22afe715c0223e56
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-05-10 16:00:49 +0200

    added failuretestresult

---
+++ Scanner-Core/src/main/java/de/rub/nds/scanner/core/constants/FailureResult.java
@@ -0,0 +1,28 @@
+/**
+ * Scanner-Core - A TLS configuration and analysis tool based on TLS-Attacker
+ *
+ * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package de.rub.nds.scanner.core.constants;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "result")
+@XmlAccessorType(XmlAccessType.FIELD)
+public enum FailureResult implements TestResult{
+
+	MISSING_CLIENT_HELLO;
+	
+    private FailureResult() {
+    }
+
+    public static TestResults of(boolean value) {
+        return TestResults.FALSE;
+    }
+}
