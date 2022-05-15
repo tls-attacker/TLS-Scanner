94640428fbb13f4790ea6b2ed2961515ef5ba914
Jia-Lin Shi <shi.jialin98@gmail.com>
2022-05-15 02:12:03 +0200

    adapted client container report creator

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/report/ClientContainerReportCreator.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/report/ClientContainerReportCreator.java
@@ -9,6 +9,7 @@
 
 package de.rub.nds.tlsscanner.clientscanner.report;
 
+import de.rub.nds.scanner.core.constants.ListResult;
 import de.rub.nds.scanner.core.report.AnsiColor;
 import de.rub.nds.scanner.core.report.ReportCreator;
 import de.rub.nds.scanner.core.report.container.HeadlineContainer;
@@ -33,10 +34,11 @@ public class ClientContainerReportCreator extends ReportCreator {
         return rootContainer;
     }
 
-    private void addCipherSuiteContainer(ListContainer rootContainer, ClientReport report) {
+    @SuppressWarnings("unchecked")
+	private void addCipherSuiteContainer(ListContainer rootContainer, ClientReport report) {
         rootContainer.add(new HeadlineContainer("Supported Cipher suites"));
         ListContainer listContainer = new ListContainer();
-        for (CipherSuite suite : report.getAdvertisedCipherSuites()) {
+        for (CipherSuite suite : ((ListResult<CipherSuite>) report.getResultMap().get(TlsAnalyzedProperty.LIST_ADVERTISED_CIPHERSUITES.name())).getList()) {
             listContainer.add(new TextContainer(suite.name(), getColorForCipherSuite(suite)));
         }
         rootContainer.add(listContainer);
