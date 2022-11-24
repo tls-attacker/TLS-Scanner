651429ed9c467f702f14aefbed2f522e7b7ecc3a
Jia-Lin <Jia-Lin.Shi@rub.de>
2022-11-24 15:00:36 +0100

    added TlsReport content to TlsScanReport

--- TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/report/TlsScanReport.java
+++ TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/report/TlsScanReport.java
@@ -8,52 +8,46 @@
  */
 package de.rub.nds.tlsscanner.core.report;
 
-import de.rub.nds.scanner.core.report.ScanReport;
-import de.rub.nds.tlsattacker.core.constants.CipherSuite;
-import de.rub.nds.tlsattacker.core.constants.CompressionMethod;
-import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
-import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
-import de.rub.nds.tlsscanner.core.constants.ProtocolType;
-import de.rub.nds.tlsscanner.core.leak.PaddingOracleTestInfo;
-import de.rub.nds.tlsscanner.core.probe.certificate.CertificateChain;
-import de.rub.nds.tlsscanner.core.probe.padding.KnownPaddingOracleVulnerability;
-import de.rub.nds.tlsscanner.core.probe.result.VersionSuiteListPair;
-import de.rub.nds.tlsscanner.core.vector.statistics.InformationLeakTest;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import de.rub.nds.scanner.core.constants.ListResult;
+import de.rub.nds.scanner.core.constants.MapResult;
+import de.rub.nds.scanner.core.constants.SetResult;
+import de.rub.nds.scanner.core.report.ScanReport;
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.constants.CompressionMethod;
+import de.rub.nds.tlsattacker.core.constants.ExtensionType;
+import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
+import de.rub.nds.tlsattacker.core.constants.NamedGroup;
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.core.constants.SignatureAndHashAlgorithm;
+import de.rub.nds.tlsattacker.core.constants.TokenBindingKeyParameters;
+import de.rub.nds.tlsattacker.core.constants.TokenBindingVersion;
+import de.rub.nds.tlsattacker.core.https.header.HttpsHeader;
+import de.rub.nds.tlsscanner.core.constants.ProtocolType;
+import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
+import de.rub.nds.tlsscanner.core.leak.PaddingOracleTestInfo;
+import de.rub.nds.tlsscanner.core.probe.certificate.CertificateChain;
+import de.rub.nds.tlsscanner.core.probe.padding.KnownPaddingOracleVulnerability;
+import de.rub.nds.tlsscanner.core.probe.result.VersionSuiteListPair;
+import de.rub.nds.tlsscanner.core.vector.statistics.InformationLeakTest;
+
+@SuppressWarnings("unchecked")
 public abstract class TlsScanReport extends ScanReport {
 
     private ProtocolType protocolType = null;
 
-    // Version
-    private List<ProtocolVersion> versions = null;
-
-    // Ciphers
-    private List<VersionSuiteListPair> versionSuitePairs = null;
-    private Set<CipherSuite> cipherSuites = null;
-
-    // Compression
-    private List<CompressionMethod> supportedCompressionMethods = null;
-
-    // Attacks
-    private List<InformationLeakTest<PaddingOracleTestInfo>> paddingOracleTestResultList;
     private KnownPaddingOracleVulnerability knownPaddingOracleVulnerability = null;
 
     // Extensions
     private List<String> supportedAlpns = null;
 
-    // Certificate
-    private List<CertificateChain> certificateChainList;
-
     // DTLS
     private Integer totalReceivedRetransmissions = 0;
-    private Map<HandshakeMessageType, Integer> retransmissionCounters;
-
-    // Entropy
-    private List<EntropyReport> entropyReportList;
 
     // Scan Timestamps
     private long scanStartTime;
@@ -71,55 +65,6 @@ public abstract class TlsScanReport extends ScanReport {
         this.protocolType = protocolType;
     }
 
-    public synchronized List<ProtocolVersion> getVersions() {
-        return versions;
-    }
-
-    public synchronized void setVersions(List<ProtocolVersion> supportedVersions) {
-        this.versions = supportedVersions;
-    }
-
-    public synchronized List<VersionSuiteListPair> getVersionSuitePairs() {
-        return versionSuitePairs;
-    }
-
-    public synchronized void setVersionSuitePairs(List<VersionSuiteListPair> versionSuitePairs) {
-        this.versionSuitePairs = versionSuitePairs;
-    }
-
-    public synchronized Set<CipherSuite> getCipherSuites() {
-        return cipherSuites;
-    }
-
-    public synchronized void setCipherSuites(Set<CipherSuite> cipherSuites) {
-        this.cipherSuites = cipherSuites;
-    }
-
-    public synchronized void addCipherSuites(Set<CipherSuite> cipherSuites) {
-        if (this.cipherSuites == null) {
-            this.cipherSuites = new HashSet<>();
-        }
-        this.cipherSuites.addAll(cipherSuites);
-    }
-
-    public synchronized List<CompressionMethod> getSupportedCompressionMethods() {
-        return supportedCompressionMethods;
-    }
-
-    public synchronized void setSupportedCompressionMethods(
-            List<CompressionMethod> supportedCompressionMethods) {
-        this.supportedCompressionMethods = supportedCompressionMethods;
-    }
-
-    public synchronized List<InformationLeakTest<PaddingOracleTestInfo>>
-            getPaddingOracleTestResultList() {
-        return paddingOracleTestResultList;
-    }
-
-    public synchronized void setPaddingOracleTestResultList(
-            List<InformationLeakTest<PaddingOracleTestInfo>> paddingOracleTestResultList) {
-        this.paddingOracleTestResultList = paddingOracleTestResultList;
-    }
 
     public synchronized KnownPaddingOracleVulnerability getKnownPaddingOracleVulnerability() {
         return knownPaddingOracleVulnerability;
@@ -138,13 +83,6 @@ public abstract class TlsScanReport extends ScanReport {
         this.supportedAlpns = supportedAlpns;
     }
 
-    public synchronized List<CertificateChain> getCertificateChainList() {
-        return certificateChainList;
-    }
-
-    public synchronized void setCertificateChainList(List<CertificateChain> certificateChainList) {
-        this.certificateChainList = certificateChainList;
-    }
 
     public synchronized Integer getTotalReceivedRetransmissions() {
         return totalReceivedRetransmissions;
@@ -152,23 +90,6 @@ public abstract class TlsScanReport extends ScanReport {
 
     public synchronized void setTotalReceivedRetransmissions(Integer totalReceivedRetransmissions) {
         this.totalReceivedRetransmissions = totalReceivedRetransmissions;
-    }
-
-    public synchronized Map<HandshakeMessageType, Integer> getRetransmissionCounters() {
-        return retransmissionCounters;
-    }
-
-    public synchronized void setRetransmissionCounters(
-            Map<HandshakeMessageType, Integer> retransmissionCounters) {
-        this.retransmissionCounters = retransmissionCounters;
-    }
-
-    public synchronized List<EntropyReport> getEntropyReportList() {
-        return entropyReportList;
-    }
-
-    public synchronized void setEntropyReportList(List<EntropyReport> entropyReportList) {
-        this.entropyReportList = entropyReportList;
     }
 
     public synchronized long getScanStartTime() {
@@ -186,4 +107,213 @@ public abstract class TlsScanReport extends ScanReport {
     public synchronized void setScanEndTime(long scanEndTime) {
         this.scanEndTime = scanEndTime;
     }
+    
+    @SuppressWarnings("rawtypes")
+    public synchronized List getPaddingOracleTestResultList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.PADDINGORACLE_TEST_RESULT);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getBleichenbacherTestResultList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.BLEICHENBACHER_TEST_RESULT);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getRaccoonTestResultList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.DIRECTRACCOON_TEST_RESULT);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getRaccoonAttackProbabilities() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.RACCOON_ATTACK_PROBABILITIES);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getInvalidCurveTestResultList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.INVALIDCURVE_TEST_RESULT);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getCcaTestResultList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.CCA_TEST_RESULTS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getNormalHpkpPins() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.NORMAL_HPKP_PINS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getReportOnlyHpkpPins() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.REPORT_ONLY_HPKP_PINS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getSimulatedClientsResultList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.CLIENT_SIMULATION_RESULTS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getCertificateChainList() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.CERTIFICATE_CHAINS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getGuidelineReports() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.GUIDELINE_REPORTS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getSupportedApplicationProtocols() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_APPLICATIONS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized List getEntropyReports() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.ENTROPY_REPORTS);
+        return listResult == null ? null : listResult.getList();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized Set getCommonDhValues() {
+        SetResult<?> setResult = getSetResult(TlsAnalyzedProperty.COMMON_DH_VALUES);
+        return setResult == null ? null : setResult.getSet();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized Map getSupportedNamedGroupsWitnesses() {
+        MapResult<?, ?> mapResult = getMapResult(TlsAnalyzedProperty.SUPPORTED_NAMED_GROUPS_WITNESSES);
+        return mapResult == null ? null : mapResult.getMap();
+    }
+
+    @SuppressWarnings("rawtypes")
+    public synchronized Map getSupportedNamedGroupsWitnessesTls13() {
+        MapResult<?, ?> mapResult = getMapResult(TlsAnalyzedProperty.SUPPORTED_NAMED_GROUPS_WITNESSES_TLS13);
+        return mapResult == null ? null : mapResult.getMap();
+    }
+
+    public synchronized Map<HandshakeMessageType, Integer> getRetransmissionCounters() {
+        MapResult<?, ?> mapResult = getMapResult(TlsAnalyzedProperty.MAP_RETRANSMISSION_COUNTERS);
+        return mapResult == null ? null : (Map<HandshakeMessageType, Integer>) mapResult.getMap();
+    }
+
+    public synchronized Set<CipherSuite> getSupportedCipherSuites() {
+        SetResult<?> setResult = getSetResult(TlsAnalyzedProperty.SUPPORTED_CIPHERSUITES);
+        return setResult == null ? null : (Set<CipherSuite>) setResult.getSet();
+    }
+
+    
+	public synchronized List<CipherSuite> getClientAdvertisedCiphersuites() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.CLIENT_ADVERTISED_CIPHERSUITES);
+        return listResult == null ? null : (List<CipherSuite>) listResult.getList();
+    }
+
+    public synchronized List<VersionSuiteListPair> getVersionSuitePairs() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.VERSION_SUITE_PAIRS);
+        return listResult == null ? null : (List<VersionSuiteListPair>) listResult.getList();
+    }
+
+    public synchronized List<ProtocolVersion> getSupportedProtocolVersions() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_PROTOCOL_VERSIONS);
+        return listResult == null ? null : (List<ProtocolVersion>) listResult.getList();
+    }
+
+    public List<SignatureAndHashAlgorithm> getSupportedSignatureAndHashAlgorithmsCert() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_SIGNATURE_AND_HASH_ALGORITHMS_CERT);
+        return listResult == null ? null : (List<SignatureAndHashAlgorithm>) listResult.getList();
+    }
+
+    public List<SignatureAndHashAlgorithm> getSupportedSignatureAndHashAlgorithmsSke() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_SIGNATURE_AND_HASH_ALGORITHMS_SKE);
+        return listResult == null ? null : (List<SignatureAndHashAlgorithm>) listResult.getList();
+    }
+
+    public List<SignatureAndHashAlgorithm> getSupportedSignatureAndHashAlgorithmsTls13() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_SIGNATURE_AND_HASH_ALGORITHMS_TLS13);
+        return listResult == null ? null : (List<SignatureAndHashAlgorithm>) listResult.getList();
+    }
+
+    public synchronized List<SignatureAndHashAlgorithm> getSupportedSignatureAndHashAlgorithms() {
+        HashSet<SignatureAndHashAlgorithm> combined = new HashSet<>();
+        if (getSupportedSignatureAndHashAlgorithmsCert() != null) {
+            combined.addAll(getSupportedSignatureAndHashAlgorithmsCert());
+        }
+        if (getSupportedSignatureAndHashAlgorithmsSke() != null) {
+            combined.addAll(getSupportedSignatureAndHashAlgorithmsSke());
+        }
+        return new ArrayList<>(combined);
+    }
+
+    public synchronized List<ExtensionType> getSupportedExtensions() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_EXTENSIONS);
+        return listResult == null ? null : (List<ExtensionType>) listResult.getList();
+    }
+
+    public synchronized List<CompressionMethod> getSupportedCompressionMethods() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_COMPRESSION_METHODS);
+        return listResult == null ? null : (List<CompressionMethod>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getSupportedTls13Groups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_TLS13_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getSupportedNamedGroups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_NAMED_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getEphemeralEcdsaPkgGroups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.EPHEMERAL_ECDSA_PK_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getTls13EcdsaPkgGroups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.TLS13_ECDSA_PK_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getStaticEcdsaSigGroups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.STATIC_ECDSA_SIG_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getEphemeralEcdsaSigGroups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.EPHEMERAL_ECDSA_SIG_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<NamedGroup> getTls13EcdsaSigGroups() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.TLS13_ECDSA_SIG_GROUPS);
+        return listResult == null ? null : (List<NamedGroup>) listResult.getList();
+    }
+
+    public synchronized List<HttpsHeader> getHttpsHeader() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.HTTPS_HEADER);
+        return listResult == null ? null : (List<HttpsHeader>) listResult.getList();
+    }
+
+    public synchronized List<TokenBindingVersion> getSupportedTokenbindingVersions() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_TOKENBINDING_VERSIONS);
+        return listResult == null ? null : (List<TokenBindingVersion>) listResult.getList();
+    }
+
+    public synchronized List<TokenBindingKeyParameters> getSupportedTokenbindingKeyParameters() {
+        ListResult<?> listResult = getListResult(TlsAnalyzedProperty.SUPPORTED_TOKENBINDING_KEY_PARAMETERS);
+        return listResult == null ? null : (List<TokenBindingKeyParameters>) listResult.getList();
+    }
+
 }
