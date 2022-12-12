8046e0cf68b0de264fa094f7a6efa4849fa5a68a
Marten Schmidt <martenls@mail.upb.de>
2022-12-12 15:45:47 +0100

    formatting

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/HeartbleedProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/HeartbleedProbe.java
@@ -1,12 +1,11 @@
-/**
- * TLS-Server-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
+/*
+ * TLS-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
  *
- * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-
 package de.rub.nds.tlsscanner.serverscanner.probe;
 
 import de.rub.nds.modifiablevariable.util.Modifiable;
@@ -29,7 +28,8 @@ import de.rub.nds.tlsscanner.serverscanner.probe.result.HeartbleedResult;
 import de.rub.nds.tlsscanner.serverscanner.report.ServerReport;
 import de.rub.nds.tlsscanner.serverscanner.selector.ConfigSelector;
 
-public class HeartbleedProbe extends TlsServerProbe<ConfigSelector, ServerReport, HeartbleedResult> {
+public class HeartbleedProbe
+        extends TlsServerProbe<ConfigSelector, ServerReport, HeartbleedResult> {
 
     public HeartbleedProbe(ConfigSelector configSelector, ParallelExecutor parallelExecutor) {
         super(parallelExecutor, TlsProbeType.HEARTBLEED, configSelector);
@@ -46,9 +46,11 @@ public class HeartbleedProbe extends TlsServerProbe<ConfigSelector, ServerReport
 
         State state = new State(tlsConfig, getTrace(tlsConfig));
         executeState(state);
-        if (WorkflowTraceUtil.didReceiveMessage(ProtocolMessageType.HEARTBEAT, state.getWorkflowTrace())) {
+        if (WorkflowTraceUtil.didReceiveMessage(
+                ProtocolMessageType.HEARTBEAT, state.getWorkflowTrace())) {
             return TestResults.TRUE;
-        } else if (!WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
+        } else if (!WorkflowTraceUtil.didReceiveMessage(
+                HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
             return TestResults.UNCERTAIN;
         } else {
             return TestResults.FALSE;
@@ -56,17 +58,23 @@ public class HeartbleedProbe extends TlsServerProbe<ConfigSelector, ServerReport
     }
 
     private WorkflowTrace getTrace(Config tlsConfig) {
-        WorkflowTrace trace = new WorkflowConfigurationFactory(tlsConfig)
-            .createWorkflowTrace(WorkflowTraceType.DYNAMIC_HANDSHAKE, RunningModeType.CLIENT);
-        HeartbeatMessage heartbeatMessage = new HeartbeatMessage(tlsConfig);
+        WorkflowTrace trace =
+                new WorkflowConfigurationFactory(tlsConfig)
+                        .createWorkflowTrace(
+                                WorkflowTraceType.DYNAMIC_HANDSHAKE, RunningModeType.CLIENT);
+        HeartbeatMessage heartbeatMessage = new HeartbeatMessage();
         // The payload consists of arbitrary content. We just set it to 5 "A" bytes.
-        heartbeatMessage.setPayload(Modifiable.explicit(new byte[] { 65, 65, 65, 65, 65 }));
+        heartbeatMessage.setPayload(Modifiable.explicit(new byte[] {65, 65, 65, 65, 65}));
         // The sender of a HeartbeatMessage MUST use a random padding of at least 16 bytes.
         // The padding of a received HeartbeatMessage message MUST be ignored. We set the padding
         // to 16 "P" bytes.
         heartbeatMessage.setPadding(
-            Modifiable.explicit(new byte[] { 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80 }));
-        // The length of the payload we want to override. We have 5 bytes of content and 16 bytes of padding. To not be
+                Modifiable.explicit(
+                        new byte[] {
+                            80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80
+                        }));
+        // The length of the payload we want to override. We have 5 bytes of content and 16 bytes of
+        // padding. To not be
         // very offensive, we set it to 22 which forces the server to leak one byte.
         heartbeatMessage.setPayloadLength(Modifiable.explicit(22));
         trace.addTlsAction(new SendAction(heartbeatMessage));
@@ -76,12 +84,12 @@ public class HeartbleedProbe extends TlsServerProbe<ConfigSelector, ServerReport
 
     @Override
     public boolean canBeExecuted(ServerReport report) {
-        return report.isProbeAlreadyExecuted(TlsProbeType.EXTENSIONS) && !report.getSupportedExtensions().isEmpty();
+        return report.isProbeAlreadyExecuted(TlsProbeType.EXTENSIONS)
+                && !report.getSupportedExtensions().isEmpty();
     }
 
     @Override
-    public void adjustConfig(ServerReport report) {
-    }
+    public void adjustConfig(ServerReport report) {}
 
     @Override
     public HeartbleedResult getCouldNotExecuteResult() {
