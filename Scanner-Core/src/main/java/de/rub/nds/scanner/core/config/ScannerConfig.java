8baeb1e01d7fa8db293669f22512037843914f4a
NErinola <nurullah.erinola@rub.de>
2022-12-22 13:29:18 +0100

    Added helper functions

--- Scanner-Core/src/main/java/de/rub/nds/scanner/core/config/ScannerConfig.java
+++ Scanner-Core/src/main/java/de/rub/nds/scanner/core/config/ScannerConfig.java
@@ -1,12 +1,11 @@
-/**
- * Scanner-Core - A TLS configuration and analysis tool based on TLS-Attacker
+/*
+ * TLS-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
  *
- * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ * Copyright 2017-2022 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-
 package de.rub.nds.scanner.core.config;
 
 import com.beust.jcommander.Parameter;
@@ -15,25 +14,39 @@ import de.rub.nds.scanner.core.constants.ScannerDetail;
 import de.rub.nds.tlsattacker.core.config.TLSDelegateConfig;
 import de.rub.nds.tlsattacker.core.config.delegate.GeneralDelegate;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class ScannerConfig extends TLSDelegateConfig {
 
-    @Parameter(names = "-noColor", required = false, description = "If you use Windows or don't want colored text.")
+    @Parameter(
+            names = "-noColor",
+            required = false,
+            description = "If you use Windows or don't want colored text.")
     private boolean noColor = false;
 
-    @Parameter(names = "-scanDetail", required = false, description = "How detailed do you want to scan?")
+    @Parameter(
+            names = "-scanDetail",
+            required = false,
+            description = "How detailed do you want to scan?")
     private ScannerDetail scanDetail = ScannerDetail.NORMAL;
 
-    @Parameter(names = "-reportDetail", required = false, description = "How detailed do you want the report to be?")
+    @Parameter(
+            names = "-reportDetail",
+            required = false,
+            description = "How detailed do you want the report to be?")
     private ScannerDetail reportDetail = ScannerDetail.NORMAL;
 
-    @Parameter(names = "-outputFile", required = false,
-        description = "Specify a file to write the site report in JSON to")
+    @Parameter(
+            names = "-outputFile",
+            required = false,
+            description = "Specify a file to write the site report in JSON to")
     private String outputFile = null;
 
-    @Parameter(names = "-probeTimeout", required = false,
-        description = "The timeout for each probe in ms (default 1800000)")
+    @Parameter(
+            names = "-probeTimeout",
+            required = false,
+            description = "The timeout for each probe in ms (default 1800000)")
     private int probeTimeout = 1800000;
 
     protected List<ProbeType> probes = null;
@@ -76,6 +89,20 @@ public abstract class ScannerConfig extends TLSDelegateConfig {
 
     public void setProbes(ProbeType... probes) {
         this.probes = Arrays.asList(probes);
+    }
+
+    public void addProbes(List<ProbeType> probes) {
+        if (this.probes == null) {
+            this.probes = new LinkedList<>();
+        }
+        this.probes.addAll(probes);
+    }
+
+    public void addProbes(ProbeType... probes) {
+        if (this.probes == null) {
+            this.probes = new LinkedList<>();
+        }
+        this.probes.addAll(Arrays.asList(probes));
     }
 
     public int getProbeTimeout() {
