0c84f4ce23afc3df56e6e7d38063ab284451be1a
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 19:25:39 +0100

    adapted probe dtls fragmentation

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/DtlsFragmentationProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/DtlsFragmentationProbe.java
@@ -10,6 +10,7 @@ package de.rub.nds.tlsscanner.clientscanner.probe;
 
 import de.rub.nds.scanner.core.constants.TestResult;
 import de.rub.nds.scanner.core.constants.TestResults;
+import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.MaxFragmentLength;
 import de.rub.nds.tlsattacker.core.protocol.message.CertificateMessage;
@@ -29,146 +30,161 @@ import de.rub.nds.tlsattacker.core.workflow.action.SendDynamicServerKeyExchangeA
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
+import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
-import de.rub.nds.tlsscanner.core.probe.result.DtlsFragmentationResult;
 
-public class DtlsFragmentationProbe
-        extends TlsClientProbe<
-                ClientScannerConfig, ClientReport, DtlsFragmentationResult<ClientReport>> {
+public class DtlsFragmentationProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
 
-    private static final int INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN = 200;
+	private static final int INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN = 200;
 
-    public DtlsFragmentationProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
-        super(executor, TlsProbeType.DTLS_FRAGMENTATION, scannerConfig);
-    }
+	private TestResult supportsDirectly;
+	private TestResult supportsDirectlyIndPackets;
+	private TestResult supportsAfterCookieExchange;
+	private TestResult supportsAfterCookieExchangeIndPackets;
+	private TestResult supportsWithExtension;
+	private TestResult supportsWithExtensionIndPackets;
 
-    @Override
-    public DtlsFragmentationResult executeTest() {
-        TestResult supportsDirectly = supportsFragmentationDirectly(false);
-        TestResult supportsDirectlyIndPackets = supportsFragmentationDirectly(true);
-        TestResult supportsAfterCookieExchange = supportsFragmentationAfterCookieExchange(false);
-        TestResult supportsAfterCookieExchangeIndPackets =
-                supportsFragmentationAfterCookieExchange(true);
-        TestResult supportsWithExtension = supportsFragmentationWithExtension(false);
-        TestResult supportsWithExtensionIndPackets = supportsFragmentationWithExtension(true);
-        return new DtlsFragmentationResult(
-                supportsDirectly,
-                supportsDirectlyIndPackets,
-                supportsAfterCookieExchange,
-                supportsAfterCookieExchangeIndPackets,
-                supportsWithExtension,
-                supportsWithExtensionIndPackets);
-    }
+	public DtlsFragmentationProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
+		super(executor, TlsProbeType.DTLS_FRAGMENTATION, scannerConfig);
+		register(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION,
+				TlsAnalyzedProperty.DTLS_FRAGMENTATION_REQUIRES_EXTENSION,
+				TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS,
+				TlsAnalyzedProperty.DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS_REQUIRES_EXTENSION);
+	}
 
-    private TestResult supportsFragmentationDirectly(boolean individualTransportPackets) {
-        Config config = scannerConfig.createConfig();
-        config.setDtlsMaximumFragmentLength(15);
-        if (individualTransportPackets) {
-            config.setCreateIndividualTransportPackets(true);
-            config.setIndividualTransportPacketCooldown(INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN);
-        }
+	@Override
+	public void executeTest() {
+		supportsDirectly = supportsFragmentationDirectly(false);
+		supportsDirectlyIndPackets = supportsFragmentationDirectly(true);
+		supportsAfterCookieExchange = supportsFragmentationAfterCookieExchange(false);
+		supportsAfterCookieExchangeIndPackets = supportsFragmentationAfterCookieExchange(true);
+		supportsWithExtension = supportsFragmentationWithExtension(false);
+		supportsWithExtensionIndPackets = supportsFragmentationWithExtension(true);
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
+	private TestResult supportsFragmentationDirectly(boolean individualTransportPackets) {
+		Config config = scannerConfig.createConfig();
+		config.setDtlsMaximumFragmentLength(15);
+		if (individualTransportPackets) {
+			config.setCreateIndividualTransportPackets(true);
+			config.setIndividualTransportPacketCooldown(INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN);
+		}
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config)
+				.createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
 
-    private TestResult supportsFragmentationAfterCookieExchange(
-            boolean individualTransportPackets) {
-        Config config = scannerConfig.createConfig();
-        if (individualTransportPackets) {
-            config.setCreateIndividualTransportPackets(true);
-            config.setIndividualTransportPacketCooldown(INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN);
-        }
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
-        trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
-        SendAction action = new SendAction(new CertificateMessage(config));
-        action.setFragments(
-                new DtlsHandshakeMessageFragment(config, 20),
-                new DtlsHandshakeMessageFragment(config, 20));
-        trace.addTlsAction(action);
-        trace.addTlsAction(new SendAction(new CertificateMessage(config)));
-        trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
-        trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	private TestResult supportsFragmentationAfterCookieExchange(boolean individualTransportPackets) {
+		Config config = scannerConfig.createConfig();
+		if (individualTransportPackets) {
+			config.setCreateIndividualTransportPackets(true);
+			config.setIndividualTransportPacketCooldown(INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN);
+		}
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config)
+				.createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
+		trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
+		SendAction action = new SendAction(new CertificateMessage(config));
+		action.setFragments(new DtlsHandshakeMessageFragment(config, 20), new DtlsHandshakeMessageFragment(config, 20));
+		trace.addTlsAction(action);
+		trace.addTlsAction(new SendAction(new CertificateMessage(config)));
+		trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
+		trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    private TestResult supportsFragmentationWithExtension(boolean individualTransportPackets) {
-        Config config = scannerConfig.createConfig();
-        config.setAddMaxFragmentLengthExtension(true);
-        config.setDefaultMaxFragmentLength(MaxFragmentLength.TWO_11);
-        if (individualTransportPackets) {
-            config.setCreateIndividualTransportPackets(true);
-            config.setIndividualTransportPacketCooldown(INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN);
-        }
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
-        trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
-        SendAction action = new SendAction(new CertificateMessage(config));
-        action.setFragments(
-                new DtlsHandshakeMessageFragment(config, 20),
-                new DtlsHandshakeMessageFragment(config, 20));
-        trace.addTlsAction(action);
-        trace.addTlsAction(new SendAction(new CertificateMessage(config)));
-        trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
-        trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	private TestResult supportsFragmentationWithExtension(boolean individualTransportPackets) {
+		Config config = scannerConfig.createConfig();
+		config.setAddMaxFragmentLengthExtension(true);
+		config.setDefaultMaxFragmentLength(MaxFragmentLength.TWO_11);
+		if (individualTransportPackets) {
+			config.setCreateIndividualTransportPackets(true);
+			config.setIndividualTransportPacketCooldown(INDIVIDUAL_TRANSPORT_PACKET_COOLDOWN);
+		}
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config)
+				.createTlsEntryWorkflowTrace(config.getDefaultClientConnection());
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage(config)));
+		trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
+		SendAction action = new SendAction(new CertificateMessage(config));
+		action.setFragments(new DtlsHandshakeMessageFragment(config, 20), new DtlsHandshakeMessageFragment(config, 20));
+		trace.addTlsAction(action);
+		trace.addTlsAction(new SendAction(new CertificateMessage(config)));
+		trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
+		trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    @Override
-    public boolean canBeExecuted(ClientReport report) {
-        return true;
-    }
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-    @Override
-    public DtlsFragmentationResult getCouldNotExecuteResult() {
-        return new DtlsFragmentationResult(
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST);
-    }
+	@Override
+	public void adjustConfig(ClientReport report) {
+	}
 
-    @Override
-    public void adjustConfig(ClientReport report) {}
+	@Override
+	protected void mergeData(ClientReport report) {
+		if (supportsDirectly == TestResults.TRUE) {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION, TestResults.TRUE);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_REQUIRES_EXTENSION, TestResults.FALSE);
+		} else if (supportsAfterCookieExchange == TestResults.TRUE) {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION, TestResults.PARTIALLY);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_REQUIRES_EXTENSION, TestResults.FALSE);
+		} else if (supportsWithExtension == TestResults.TRUE) {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION, TestResults.PARTIALLY);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_REQUIRES_EXTENSION, TestResults.TRUE);
+		} else {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION, TestResults.FALSE);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_REQUIRES_EXTENSION, TestResults.FALSE);
+		}
+
+		if (supportsDirectlyIndPackets == TestResults.TRUE) {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS, TestResults.TRUE);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS_REQUIRES_EXTENSION, TestResults.FALSE);
+		} else if (supportsAfterCookieExchangeIndPackets == TestResults.TRUE) {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS, TestResults.PARTIALLY);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS_REQUIRES_EXTENSION, TestResults.FALSE);
+		} else if (supportsWithExtensionIndPackets == TestResults.TRUE) {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS, TestResults.PARTIALLY);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS_REQUIRES_EXTENSION, TestResults.TRUE);
+		} else {
+			put(TlsAnalyzedProperty.SUPPORTS_DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS, TestResults.FALSE);
+			put(TlsAnalyzedProperty.DTLS_FRAGMENTATION_WITH_INDIVIDUAL_PACKETS_REQUIRES_EXTENSION, TestResults.FALSE);
+		}
+	}
+
+	@Override
+	protected Requirement getRequirements() {
+		return Requirement.NO_REQUIREMENT;
+	}
+
 }
