f4a6d1877f30d7f41e1b7ed60cb597229cbca6ec
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 16:46:15 +0100

    adapted probe resumption except requirement

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ResumptionProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ResumptionProbe.java
@@ -10,6 +10,7 @@ package de.rub.nds.tlsscanner.clientscanner.probe;
 
 import de.rub.nds.scanner.core.constants.TestResult;
 import de.rub.nds.scanner.core.constants.TestResults;
+import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.AlertDescription;
 import de.rub.nds.tlsattacker.core.constants.AlertLevel;
@@ -26,133 +27,125 @@ import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
-import de.rub.nds.tlsscanner.clientscanner.probe.result.ResumptionResult;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
+import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
 import java.util.function.Function;
 
-public class ResumptionProbe
-        extends TlsClientProbe<ClientScannerConfig, ClientReport, ResumptionResult> {
+public class ResumptionProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
 
-    private TestResult supportsDtlsCookieExchangeInIdResumption;
-    private TestResult supportsDtlsCookieExchangeInTicketResumption;
+	private TestResult supportsResumption;
+	private TestResult supportsSessionTicketResumption;
+	private TestResult supportsDtlsCookieExchangeInResumption;
+	private TestResult supportsDtlsCookieExchangeInSessionTicketResumption;
 
-    public ResumptionProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
-        super(executor, TlsProbeType.RESUMPTION, scannerConfig);
-    }
+	public ResumptionProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
+		super(executor, TlsProbeType.RESUMPTION, scannerConfig);
+		register(TlsAnalyzedProperty.SUPPORTS_SESSION_ID_RESUMPTION,
+				TlsAnalyzedProperty.SUPPORTS_SESSION_TICKET_RESUMPTION,
+				TlsAnalyzedProperty.SUPPORTS_DTLS_COOKIE_EXCHANGE_IN_SESSION_ID_RESUMPTION,
+				TlsAnalyzedProperty.SUPPORTS_DTLS_COOKIE_EXCHANGE_IN_SESSION_TICKET_RESUMPTION);
+	}
 
-    @Override
-    public ResumptionResult executeTest() {
-        Function<State, Integer> beforeTransportInitCallback =
-                getParallelExecutor().getDefaultBeforeTransportInitCallback();
-        String runCommand =
-                scannerConfig.getRunCommand().strip()
-                        + " "
-                        + scannerConfig.getClientParameterDelegate().getResumptionOptions().strip();
-        getParallelExecutor()
-                .setDefaultBeforeTransportInitCallback(
-                        scannerConfig.getRunCommandExecutionCallback(runCommand));
+	@Override
+	public void executeTest() {
+		Function<State, Integer> beforeTransportInitCallback = getParallelExecutor()
+				.getDefaultBeforeTransportInitCallback();
+		String runCommand = scannerConfig.getRunCommand().strip() + " "
+				+ scannerConfig.getClientParameterDelegate().getResumptionOptions().strip();
+		getParallelExecutor()
+				.setDefaultBeforeTransportInitCallback(scannerConfig.getRunCommandExecutionCallback(runCommand));
 
-        ResumptionResult result;
-        if (scannerConfig.getDtlsDelegate().isDTLS()) {
-            supportsDtlsCookieExchangeInIdResumption =
-                    getSupportsDtlsCookieExchangeInIdResumption();
-            supportsDtlsCookieExchangeInTicketResumption =
-                    getSupportsDtlsCookieExchangeInTicketResumption();
-            result =
-                    new ResumptionResult(
-                            getSupportsIdResumption(),
-                            getSupportsTicketResumption(),
-                            supportsDtlsCookieExchangeInIdResumption,
-                            supportsDtlsCookieExchangeInTicketResumption);
-        } else {
-            supportsDtlsCookieExchangeInIdResumption = TestResults.NOT_TESTED_YET;
-            supportsDtlsCookieExchangeInTicketResumption = TestResults.NOT_TESTED_YET;
-            result =
-                    new ResumptionResult(
-                            getSupportsIdResumption(),
-                            getSupportsTicketResumption(),
-                            supportsDtlsCookieExchangeInIdResumption,
-                            supportsDtlsCookieExchangeInTicketResumption);
-        }
+		supportsDtlsCookieExchangeInResumption = scannerConfig.getDtlsDelegate().isDTLS()
+				? getSupportsDtlsCookieExchangeInIdResumption()
+				: TestResults.NOT_TESTED_YET;
+		supportsDtlsCookieExchangeInSessionTicketResumption = scannerConfig.getDtlsDelegate().isDTLS()
+				? getSupportsDtlsCookieExchangeInTicketResumption()
+				: TestResults.NOT_TESTED_YET;
+		supportsResumption = getSupportsIdResumption();
+		supportsSessionTicketResumption = getSupportsTicketResumption();
 
-        getParallelExecutor().setDefaultBeforeTransportInitCallback(beforeTransportInitCallback);
-        return result;
-    }
+		getParallelExecutor().setDefaultBeforeTransportInitCallback(beforeTransportInitCallback);
+	}
 
-    private TestResult getSupportsDtlsCookieExchangeInIdResumption() {
-        Config config = scannerConfig.createConfig();
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createWorkflowTrace(WorkflowTraceType.HANDSHAKE, RunningModeType.SERVER);
-        addResetConnectionActions(trace);
+	private TestResult getSupportsDtlsCookieExchangeInIdResumption() {
+		Config config = scannerConfig.createConfig();
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config).createWorkflowTrace(WorkflowTraceType.HANDSHAKE,
+				RunningModeType.SERVER);
+		addResetConnectionActions(trace);
 
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage()));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-    private TestResult getSupportsIdResumption() {
-        Config config = scannerConfig.createConfig();
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createWorkflowTrace(WorkflowTraceType.HANDSHAKE, RunningModeType.SERVER);
-        addResetConnectionActions(trace);
+	private TestResult getSupportsIdResumption() {
+		Config config = scannerConfig.createConfig();
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config).createWorkflowTrace(WorkflowTraceType.HANDSHAKE,
+				RunningModeType.SERVER);
+		addResetConnectionActions(trace);
 
-        config.setDtlsCookieExchange(supportsDtlsCookieExchangeInIdResumption == TestResults.TRUE);
-        WorkflowTrace resumptionTrace =
-                new WorkflowConfigurationFactory(config)
-                        .createWorkflowTrace(WorkflowTraceType.RESUMPTION, RunningModeType.SERVER);
-        trace.addTlsActions(resumptionTrace.getTlsActions());
+		config.setDtlsCookieExchange(supportsDtlsCookieExchangeInResumption == TestResults.TRUE);
+		WorkflowTrace resumptionTrace = new WorkflowConfigurationFactory(config)
+				.createWorkflowTrace(WorkflowTraceType.RESUMPTION, RunningModeType.SERVER);
+		trace.addTlsActions(resumptionTrace.getTlsActions());
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-    private TestResult getSupportsDtlsCookieExchangeInTicketResumption() {
-        // TODO
-        return TestResults.NOT_TESTED_YET;
-    }
+	private TestResult getSupportsDtlsCookieExchangeInTicketResumption() {
+		// TODO
+		return TestResults.NOT_TESTED_YET;
+	}
 
-    private TestResult getSupportsTicketResumption() {
-        // TODO
-        return TestResults.NOT_TESTED_YET;
-    }
+	private TestResult getSupportsTicketResumption() {
+		// TODO
+		return TestResults.NOT_TESTED_YET;
+	}
 
-    private void addResetConnectionActions(WorkflowTrace trace) {
-        AlertMessage alert = new AlertMessage();
-        alert.setConfig(AlertLevel.WARNING, AlertDescription.CLOSE_NOTIFY);
-        trace.addTlsAction(new SendAction(alert));
-        trace.addTlsAction(new ResetConnectionAction());
-    }
+	private void addResetConnectionActions(WorkflowTrace trace) {
+		AlertMessage alert = new AlertMessage();
+		alert.setConfig(AlertLevel.WARNING, AlertDescription.CLOSE_NOTIFY);
+		trace.addTlsAction(new SendAction(alert));
+		trace.addTlsAction(new ResetConnectionAction());
+	}
 
-    @Override
-    public boolean canBeExecuted(ClientReport report) {
-        return scannerConfig.getClientParameterDelegate().getResumptionOptions() != null;
-    }
+	@Override
+	public boolean canBeExecuted(ClientReport report) {
+		return scannerConfig.getClientParameterDelegate().getResumptionOptions() != null;
+	}
 
-    @Override
-    public ResumptionResult getCouldNotExecuteResult() {
-        return new ResumptionResult(
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST);
-    }
+	@Override
+	public void adjustConfig(ClientReport report) {
+	}
 
-    @Override
-    public void adjustConfig(ClientReport report) {}
+	@Override
+	protected void mergeData(ClientReport report) {
+		put(TlsAnalyzedProperty.SUPPORTS_SESSION_ID_RESUMPTION, supportsResumption);
+		put(TlsAnalyzedProperty.SUPPORTS_SESSION_TICKET_RESUMPTION, supportsSessionTicketResumption);
+		put(TlsAnalyzedProperty.SUPPORTS_DTLS_COOKIE_EXCHANGE_IN_SESSION_ID_RESUMPTION,
+				supportsDtlsCookieExchangeInResumption);
+		put(TlsAnalyzedProperty.SUPPORTS_DTLS_COOKIE_EXCHANGE_IN_SESSION_TICKET_RESUMPTION,
+				supportsDtlsCookieExchangeInSessionTicketResumption);
+	}
+
+	@Override
+	protected Requirement getRequirements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
