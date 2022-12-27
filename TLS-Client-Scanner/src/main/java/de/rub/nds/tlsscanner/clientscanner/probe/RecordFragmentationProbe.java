05904ea0d64f96d18124ff958aef8eb77e3e6b06
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 19:03:05 +0100

    adapted probe record fragmentation

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/RecordFragmentationProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/RecordFragmentationProbe.java
@@ -8,6 +8,7 @@
  */
 package de.rub.nds.tlsscanner.clientscanner.probe;
 
+import de.rub.nds.scanner.core.constants.TestResult;
 import de.rub.nds.scanner.core.constants.TestResults;
 import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.tlsattacker.core.config.Config;
@@ -25,80 +26,45 @@ import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
 import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
-import de.rub.nds.tlsscanner.core.probe.result.RecordFragmentationResult;
 
-<<<<<<< HEAD:TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ClientRecordFragmentationProbe.java
-public class ClientRecordFragmentationProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
+public class RecordFragmentationProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
+	private TestResult result;
 
-    private TestResult result;
-=======
-public class RecordFragmentationProbe
-        extends TlsClientProbe<
-                ClientScannerConfig, ClientReport, RecordFragmentationResult<ClientReport>> {
->>>>>>> master:TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/RecordFragmentationProbe.java
+	public RecordFragmentationProbe(ParallelExecutor parallelExecutor, ClientScannerConfig scannerConfig) {
+		super(parallelExecutor, TlsProbeType.RECORD_FRAGMENTATION, scannerConfig);
+		register(TlsAnalyzedProperty.SUPPORTS_RECORD_FRAGMENTATION);
+	}
 
-    public RecordFragmentationProbe(
-            ParallelExecutor parallelExecutor, ClientScannerConfig scannerConfig) {
-        super(parallelExecutor, TlsProbeType.RECORD_FRAGMENTATION, scannerConfig);
-        register(TlsAnalyzedProperty.SUPPORTS_RECORD_FRAGMENTATION);
-    }
+	@Override
+	public void executeTest() {
+		Config config = scannerConfig.createConfig();
+		config.setDefaultMaxRecordData(50);
 
-    @Override
-<<<<<<< HEAD:TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ClientRecordFragmentationProbe.java
-    public void executeTest() {
-=======
-    public RecordFragmentationResult executeTest() {
->>>>>>> master:TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/RecordFragmentationProbe.java
-        Config config = scannerConfig.createConfig();
-        config.setDefaultMaxRecordData(50);
+		WorkflowConfigurationFactory factory = new WorkflowConfigurationFactory(config);
+		WorkflowTrace workflowTrace = factory.createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
+		workflowTrace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-        WorkflowConfigurationFactory factory = new WorkflowConfigurationFactory(config);
-        WorkflowTrace workflowTrace =
-                factory.createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
-        workflowTrace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+		State state = new State(config, workflowTrace);
+		executeState(state);
 
-        State state = new State(config, workflowTrace);
-        executeState(state);
+		result = WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, state.getWorkflowTrace())
+				? TestResults.TRUE
+				: TestResults.FALSE;
 
-<<<<<<< HEAD:TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ClientRecordFragmentationProbe.java
-        result = state.getWorkflowTrace().executedAsPlanned() ? TestResults.TRUE : TestResults.FALSE;
-    }
+	}
 
-    @Override
-    public void adjustConfig(ClientReport report) {
+	@Override
+	protected Requirement getRequirements() {
+		return Requirement.NO_REQUIREMENT;
+	}
 
-    }
+	@Override
+	public void adjustConfig(ClientReport report) {
 
-    @Override
-    protected void mergeData(ClientReport report) {
-        put(TlsAnalyzedProperty.SUPPORTS_RECORD_FRAGMENTATION, result);
-    }
+	}
 
-    @Override
-    protected Requirement getRequirements() {
-        return Requirement.NO_REQUIREMENT;
-    }
-
-=======
-        if (WorkflowTraceUtil.didReceiveMessage(
-                HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
-            return new RecordFragmentationResult(TestResults.TRUE);
-        } else {
-            return new RecordFragmentationResult(TestResults.FALSE);
-        }
-    }
-
-    @Override
-    public boolean canBeExecuted(ClientReport report) {
-        return true;
-    }
-
-    @Override
-    public RecordFragmentationResult getCouldNotExecuteResult() {
-        return new RecordFragmentationResult(null);
-    }
-
-    @Override
-    public void adjustConfig(ClientReport report) {}
->>>>>>> master:TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/RecordFragmentationProbe.java
+	@Override
+	protected void mergeData(ClientReport report) {
+		put(TlsAnalyzedProperty.SUPPORTS_RECORD_FRAGMENTATION, result);
+	}
 }
