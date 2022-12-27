fe5abf3257a3b0350b223e475a3f2f0ef300a584
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 16:03:41 +0100

    fixed probe ec point format

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/ECPointFormatProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/ECPointFormatProbe.java
@@ -6,6 +6,7 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
+
 package de.rub.nds.tlsscanner.serverscanner.probe;
 
 import de.rub.nds.modifiablevariable.util.Modifiable;
@@ -33,14 +34,8 @@ import de.rub.nds.tlsscanner.serverscanner.selector.ConfigSelector;
 import java.util.LinkedList;
 import java.util.List;
 
-<<<<<<< HEAD
 public class ECPointFormatProbe extends TlsServerProbe<ConfigSelector, ServerReport> {
     private static final byte[] UNDEFINED_FORMAT = new byte[] { (byte) 0xE4, (byte) 0x04 };
-=======
-public class ECPointFormatProbe
-        extends TlsServerProbe<ConfigSelector, ServerReport, ECPointFormatResult> {
-    private static final byte[] UNDEFINED_FORMAT = new byte[] {(byte) 0xE4, (byte) 0x04};
->>>>>>> master
 
     private Boolean shouldTestTls13;
     private Boolean shouldTestPointFormats;
@@ -68,26 +63,9 @@ public class ECPointFormatProbe
             supportedFormats = getSupportedPointFormats();
             completesHandshakeWithUndefined = canHandshakeWithUndefinedFormat();
         }
-<<<<<<< HEAD
         tls13SecpCompression = shouldTestTls13 ? getTls13SecpCompressionSupported() : TestResults.COULD_NOT_TEST;
         if (supportedFormats == null) {
             LOGGER.debug("Unable to determine supported point formats");
-=======
-        TestResult tls13SecpCompressionSupported;
-        if (shouldTestTls13) {
-            tls13SecpCompressionSupported = getTls13SecpCompressionSupported();
-        } else {
-            tls13SecpCompressionSupported = TestResults.COULD_NOT_TEST;
-        }
-        if (pointFormats != null) {
-            return (new ECPointFormatResult(
-                    pointFormats, tls13SecpCompressionSupported, completedWithUndefined));
-
-        } else {
-            LOGGER.debug("Unable to determine supported point formats");
-            return (new ECPointFormatResult(
-                    null, tls13SecpCompressionSupported, completedWithUndefined));
->>>>>>> master
         }
     }
 
@@ -104,13 +82,10 @@ public class ECPointFormatProbe
         List<CipherSuite> ourECDHCipherSuites = getCipherSuitesForTest();
         List<NamedGroup> groups = getGroupsForTest(dummyFormat);
         State state = getState(ourECDHCipherSuites, dummyFormat, groups);
-        state.getWorkflowTrace()
-                .getFirstSendMessage(ClientHelloMessage.class)
-                .getExtension(ECPointFormatExtensionMessage.class)
-                .setPointFormats(Modifiable.explicit(UNDEFINED_FORMAT));
+        state.getWorkflowTrace().getFirstSendMessage(ClientHelloMessage.class)
+            .getExtension(ECPointFormatExtensionMessage.class).setPointFormats(Modifiable.explicit(UNDEFINED_FORMAT));
         executeState(state);
-        if (WorkflowTraceUtil.didReceiveMessage(
-                HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
+        if (WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
             return TestResults.TRUE;
         }
         return TestResults.FALSE;
@@ -122,8 +97,7 @@ public class ECPointFormatProbe
         List<NamedGroup> groups = getGroupsForTest(format);
         State state = getState(ourECDHCipherSuites, format, groups);
         executeState(state);
-        if (WorkflowTraceUtil.didReceiveMessage(
-                HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
+        if (WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
             supportedFormats.add(format);
         }
     }
@@ -157,8 +131,7 @@ public class ECPointFormatProbe
         return groups;
     }
 
-    public State getState(
-            List<CipherSuite> ourECDHCipherSuites, ECPointFormat format, List<NamedGroup> groups) {
+    public State getState(List<CipherSuite> ourECDHCipherSuites, ECPointFormat format, List<NamedGroup> groups) {
         Config config = configSelector.getBaseConfig();
         config.setWorkflowTraceType(WorkflowTraceType.DYNAMIC_HANDSHAKE);
         config.setDefaultClientSupportedCipherSuites(ourECDHCipherSuites);
@@ -182,14 +155,12 @@ public class ECPointFormatProbe
             tlsConfig.setWorkflowTraceType(WorkflowTraceType.DYNAMIC_HANDSHAKE);
             tlsConfig.setDefaultClientNamedGroups(secpGroups);
             tlsConfig.setDefaultClientKeyShareNamedGroups(secpGroups);
-            tlsConfig.setDefaultClientSupportedPointFormats(
-                    ECPointFormat.ANSIX962_COMPRESSED_PRIME);
+            tlsConfig.setDefaultClientSupportedPointFormats(ECPointFormat.ANSIX962_COMPRESSED_PRIME);
             tlsConfig.setDefaultSelectedPointFormat(ECPointFormat.ANSIX962_COMPRESSED_PRIME);
             State state = new State(tlsConfig);
 
             executeState(state);
-            if (WorkflowTraceUtil.didReceiveMessage(
-                    HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
+            if (WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, state.getWorkflowTrace())) {
                 return TestResults.TRUE;
             }
             return TestResults.FALSE;
@@ -205,41 +176,21 @@ public class ECPointFormatProbe
     }
 
     @Override
-<<<<<<< HEAD
     protected Requirement getRequirements() {
         PropertyRequirement preq_ecdh = new PropertyRequirement(TlsAnalyzedProperty.SUPPORTS_ECDHE);
         PropertyRequirement preq_tls13 = new PropertyRequirement(TlsAnalyzedProperty.SUPPORTS_TLS_1_3);
         return new ProbeRequirement(TlsProbeType.PROTOCOL_VERSION, TlsProbeType.CIPHER_SUITE)
             .requires(new OrRequirement(preq_ecdh, preq_tls13));
-=======
-    public boolean canBeExecuted(ServerReport report) {
-        return report.isProbeAlreadyExecuted(TlsProbeType.PROTOCOL_VERSION)
-                && (report.getResult(TlsAnalyzedProperty.SUPPORTS_ECDHE) == TestResults.TRUE
-                        || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_3)
-                                == TestResults.TRUE);
-    }
-
-    @Override
-    public ECPointFormatResult getCouldNotExecuteResult() {
-        return new ECPointFormatResult(
-                null, TestResults.COULD_NOT_TEST, TestResults.COULD_NOT_TEST);
->>>>>>> master
     }
 
     @Override
     public void adjustConfig(ServerReport report) {
-        shouldTestPointFormats =
-                report.getResult(TlsAnalyzedProperty.SUPPORTS_DTLS_1_0) == TestResults.TRUE
-                        || report.getResult(TlsAnalyzedProperty.SUPPORTS_DTLS_1_2)
-                                == TestResults.TRUE
-                        || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_2)
-                                == TestResults.TRUE
-                        || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_1)
-                                == TestResults.TRUE
-                        || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_0)
-                                == TestResults.TRUE;
-        shouldTestTls13 =
-                report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_3) == TestResults.TRUE;
+        shouldTestPointFormats = report.getResult(TlsAnalyzedProperty.SUPPORTS_DTLS_1_0) == TestResults.TRUE
+            || report.getResult(TlsAnalyzedProperty.SUPPORTS_DTLS_1_2) == TestResults.TRUE
+            || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_2) == TestResults.TRUE
+            || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_1) == TestResults.TRUE
+            || report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_0) == TestResults.TRUE;
+        shouldTestTls13 = report.getResult(TlsAnalyzedProperty.SUPPORTS_TLS_1_3) == TestResults.TRUE;
     }
 
     private List<NamedGroup> getSpecificGroups(String identifier) {
