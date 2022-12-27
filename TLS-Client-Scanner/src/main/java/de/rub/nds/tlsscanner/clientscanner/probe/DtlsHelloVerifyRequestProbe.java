8dcf9fb50d7ec580718b745ea0bb0badc3488d49
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 18:23:34 +0100

    adapted probe dtls hello verify request

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/DtlsHelloVerifyRequestProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/DtlsHelloVerifyRequestProbe.java
@@ -11,6 +11,7 @@ package de.rub.nds.tlsscanner.clientscanner.probe;
 import de.rub.nds.modifiablevariable.util.Modifiable;
 import de.rub.nds.scanner.core.constants.TestResult;
 import de.rub.nds.scanner.core.constants.TestResults;
+import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
@@ -33,188 +34,175 @@ import de.rub.nds.tlsattacker.core.workflow.action.SendDynamicServerKeyExchangeA
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
-import de.rub.nds.tlsscanner.clientscanner.probe.result.DtlsHelloVerifyRequestResult;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
+import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
 import java.util.Arrays;
 
-public class DtlsHelloVerifyRequestProbe
-        extends TlsClientProbe<ClientScannerConfig, ClientReport, DtlsHelloVerifyRequestResult> {
+public class DtlsHelloVerifyRequestProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
 
-    public DtlsHelloVerifyRequestProbe(
-            ParallelExecutor executor, ClientScannerConfig scannerConfig) {
-        super(executor, TlsProbeType.DTLS_HELLO_VERIFY_REQUEST, scannerConfig);
-    }
+	private TestResult acceptsLegacyServerVersionMismatch;
+	private TestResult acceptsHvrSequenceNumberMismatch;
+	private TestResult acceptsServerHelloSequenceNumberMismatch;
+	private TestResult hasClientHelloMismatch;
+	private TestResult acceptsEmptyCookie;
 
-    @Override
-    public DtlsHelloVerifyRequestResult executeTest() {
-        return new DtlsHelloVerifyRequestResult(
-                acceptsLegacyServerVersionMismatch(),
-                acceptsHvrSequenceNumberMismatch(),
-                acceptsServerHelloSequenceNumberMismatch(),
-                hasClientHelloMismatch(),
-                acceptsEmptyCookie());
-    }
+	public DtlsHelloVerifyRequestProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
+		super(executor, TlsProbeType.DTLS_HELLO_VERIFY_REQUEST, scannerConfig);
+		register(TlsAnalyzedProperty.ACCEPTS_HVR_LEGACY_SERVER_VERSION_MISMATCH,
+				TlsAnalyzedProperty.ACCEPTS_HVR_RECORD_SEQUENCE_NUMBER_MISMATCH,
+				TlsAnalyzedProperty.ACCEPTS_SERVER_HELLO_RECORD_SEQUENCE_NUMBER_MISMATCH,
+				TlsAnalyzedProperty.HAS_CLIENT_HELLO_MISMATCH, TlsAnalyzedProperty.ACCEPTS_EMPTY_COOKIE);
+	}
 
-    private TestResult acceptsLegacyServerVersionMismatch() {
-        Config config = scannerConfig.createConfig();
+	@Override
+	public void executeTest() {
+		acceptsLegacyServerVersionMismatch = acceptsLegacyServerVersionMismatch();
+		acceptsHvrSequenceNumberMismatch = acceptsHvrSequenceNumberMismatch();
+		acceptsServerHelloSequenceNumberMismatch = acceptsServerHelloSequenceNumberMismatch();
+		hasClientHelloMismatch = hasClientHelloMismatch();
+		acceptsEmptyCookie = acceptsEmptyCookie();
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
-        HelloVerifyRequestMessage hvrMessage = new HelloVerifyRequestMessage(config);
-        hvrMessage.setProtocolVersion(Modifiable.explicit(ProtocolVersion.DTLS10.getValue()));
-        WorkflowTraceMutator.replaceSendingMessage(
-                trace, HandshakeMessageType.HELLO_VERIFY_REQUEST, hvrMessage);
-        ServerHelloMessage serverHello = new ServerHelloMessage(config);
-        serverHello.setProtocolVersion(Modifiable.explicit(ProtocolVersion.DTLS12.getValue()));
-        WorkflowTraceMutator.replaceSendingMessage(
-                trace, HandshakeMessageType.SERVER_HELLO, serverHello);
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	private TestResult acceptsLegacyServerVersionMismatch() {
+		Config config = scannerConfig.createConfig();
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config).createWorkflowTrace(WorkflowTraceType.HELLO,
+				RunningModeType.SERVER);
+		HelloVerifyRequestMessage hvrMessage = new HelloVerifyRequestMessage(config);
+		hvrMessage.setProtocolVersion(Modifiable.explicit(ProtocolVersion.DTLS10.getValue()));
+		WorkflowTraceMutator.replaceSendingMessage(trace, HandshakeMessageType.HELLO_VERIFY_REQUEST, hvrMessage);
+		ServerHelloMessage serverHello = new ServerHelloMessage(config);
+		serverHello.setProtocolVersion(Modifiable.explicit(ProtocolVersion.DTLS12.getValue()));
+		WorkflowTraceMutator.replaceSendingMessage(trace, HandshakeMessageType.SERVER_HELLO, serverHello);
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    private TestResult acceptsHvrSequenceNumberMismatch() {
-        Config config = scannerConfig.createConfig();
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createTlsEntryWorkflowTrace(config.getDefaultServerConnection());
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
-        trace.addTlsAction(new ChangeWriteSequenceNumberAction(5));
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage(config)));
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
-        trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
-        trace.addTlsAction(new SendDynamicServerCertificateAction());
-        trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
-        trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	private TestResult acceptsHvrSequenceNumberMismatch() {
+		Config config = scannerConfig.createConfig();
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config)
+				.createTlsEntryWorkflowTrace(config.getDefaultServerConnection());
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
+		trace.addTlsAction(new ChangeWriteSequenceNumberAction(5));
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage(config)));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
+		trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
+		trace.addTlsAction(new SendDynamicServerCertificateAction());
+		trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
+		trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    private TestResult acceptsServerHelloSequenceNumberMismatch() {
-        Config config = scannerConfig.createConfig();
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createTlsEntryWorkflowTrace(config.getDefaultServerConnection());
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage(config)));
-        trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
-        trace.addTlsAction(new ChangeWriteSequenceNumberAction(5));
-        trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
-        trace.addTlsAction(new SendDynamicServerCertificateAction());
-        trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
-        trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	private TestResult acceptsServerHelloSequenceNumberMismatch() {
+		Config config = scannerConfig.createConfig();
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config)
+				.createTlsEntryWorkflowTrace(config.getDefaultServerConnection());
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage(config)));
+		trace.addTlsAction(new ReceiveAction(new ClientHelloMessage()));
+		trace.addTlsAction(new ChangeWriteSequenceNumberAction(5));
+		trace.addTlsAction(new SendAction(new ServerHelloMessage(config)));
+		trace.addTlsAction(new SendDynamicServerCertificateAction());
+		trace.addTlsAction(new SendDynamicServerKeyExchangeAction());
+		trace.addTlsAction(new SendAction(new ServerHelloDoneMessage(config)));
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    private TestResult hasClientHelloMismatch() {
-        Config config = scannerConfig.createConfig();
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-        WorkflowTrace trace = new WorkflowTrace();
-        ReceiveAction firstReceiveAction = new ReceiveAction(new ClientHelloMessage());
-        trace.addTlsAction(firstReceiveAction);
-        trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage(config)));
-        ReceiveAction secondReceiveAction = new ReceiveAction(new ClientHelloMessage());
-        trace.addTlsAction(secondReceiveAction);
+	private TestResult hasClientHelloMismatch() {
+		Config config = scannerConfig.createConfig();
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            ClientHelloMessage firstClientHello =
-                    (ClientHelloMessage) firstReceiveAction.getReceivedMessages().get(0);
-            ClientHelloMessage secondClientHello =
-                    (ClientHelloMessage) secondReceiveAction.getReceivedMessages().get(0);
-            boolean versionMatch =
-                    Arrays.equals(
-                            firstClientHello.getProtocolVersion().getValue(),
-                            secondClientHello.getProtocolVersion().getValue());
-            boolean randomMatch =
-                    Arrays.equals(
-                            firstClientHello.getRandom().getValue(),
-                            secondClientHello.getRandom().getValue());
-            boolean sessionIdMatch =
-                    Arrays.equals(
-                            firstClientHello.getSessionId().getValue(),
-                            secondClientHello.getSessionId().getValue());
-            boolean cipherSuitesMatch =
-                    Arrays.equals(
-                            firstClientHello.getCipherSuites().getValue(),
-                            secondClientHello.getCipherSuites().getValue());
-            boolean compressionsMatch =
-                    Arrays.equals(
-                            firstClientHello.getCompressions().getValue(),
-                            secondClientHello.getCompressions().getValue());
-            if (versionMatch
-                    && randomMatch
-                    && sessionIdMatch
-                    && cipherSuitesMatch
-                    && compressionsMatch) {
-                return TestResults.FALSE;
-            } else {
-                return TestResults.TRUE;
-            }
-        } else {
-            return TestResults.ERROR_DURING_TEST;
-        }
-    }
+		WorkflowTrace trace = new WorkflowTrace();
+		ReceiveAction firstReceiveAction = new ReceiveAction(new ClientHelloMessage());
+		trace.addTlsAction(firstReceiveAction);
+		trace.addTlsAction(new SendAction(new HelloVerifyRequestMessage(config)));
+		ReceiveAction secondReceiveAction = new ReceiveAction(new ClientHelloMessage());
+		trace.addTlsAction(secondReceiveAction);
 
-    private TestResult acceptsEmptyCookie() {
-        Config config = scannerConfig.createConfig();
-        config.setDtlsDefaultCookieLength(0);
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			ClientHelloMessage firstClientHello = (ClientHelloMessage) firstReceiveAction.getReceivedMessages().get(0);
+			ClientHelloMessage secondClientHello = (ClientHelloMessage) secondReceiveAction.getReceivedMessages()
+					.get(0);
+			boolean versionMatch = Arrays.equals(firstClientHello.getProtocolVersion().getValue(),
+					secondClientHello.getProtocolVersion().getValue());
+			boolean randomMatch = Arrays.equals(firstClientHello.getRandom().getValue(),
+					secondClientHello.getRandom().getValue());
+			boolean sessionIdMatch = Arrays.equals(firstClientHello.getSessionId().getValue(),
+					secondClientHello.getSessionId().getValue());
+			boolean cipherSuitesMatch = Arrays.equals(firstClientHello.getCipherSuites().getValue(),
+					secondClientHello.getCipherSuites().getValue());
+			boolean compressionsMatch = Arrays.equals(firstClientHello.getCompressions().getValue(),
+					secondClientHello.getCompressions().getValue());
+			if (versionMatch && randomMatch && sessionIdMatch && cipherSuitesMatch && compressionsMatch) {
+				return TestResults.FALSE;
+			} else {
+				return TestResults.TRUE;
+			}
+		} else {
+			return TestResults.ERROR_DURING_TEST;
+		}
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	private TestResult acceptsEmptyCookie() {
+		Config config = scannerConfig.createConfig();
+		config.setDtlsDefaultCookieLength(0);
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (state.getWorkflowTrace().executedAsPlanned()) {
-            return TestResults.TRUE;
-        } else {
-            return TestResults.FALSE;
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config).createWorkflowTrace(WorkflowTraceType.HELLO,
+				RunningModeType.SERVER);
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    @Override
-    public boolean canBeExecuted(ClientReport report) {
-        return true;
-    }
+		State state = new State(config, trace);
+		executeState(state);
+		if (state.getWorkflowTrace().executedAsPlanned()) {
+			return TestResults.TRUE;
+		} else {
+			return TestResults.FALSE;
+		}
+	}
 
-    @Override
-    public DtlsHelloVerifyRequestResult getCouldNotExecuteResult() {
-        return new DtlsHelloVerifyRequestResult(
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST,
-                TestResults.COULD_NOT_TEST);
-    }
+	@Override
+	public void adjustConfig(ClientReport report) {
+	}
 
-    @Override
-    public void adjustConfig(ClientReport report) {}
+	@Override
+	protected void mergeData(ClientReport report) {
+		put(TlsAnalyzedProperty.ACCEPTS_HVR_LEGACY_SERVER_VERSION_MISMATCH, acceptsLegacyServerVersionMismatch);
+		put(TlsAnalyzedProperty.ACCEPTS_HVR_RECORD_SEQUENCE_NUMBER_MISMATCH, acceptsHvrSequenceNumberMismatch);
+		put(TlsAnalyzedProperty.ACCEPTS_SERVER_HELLO_RECORD_SEQUENCE_NUMBER_MISMATCH,
+				acceptsServerHelloSequenceNumberMismatch);
+		put(TlsAnalyzedProperty.HAS_CLIENT_HELLO_MISMATCH, hasClientHelloMismatch);
+		put(TlsAnalyzedProperty.ACCEPTS_EMPTY_COOKIE, acceptsEmptyCookie);
+	}
+
+	@Override
+	protected Requirement getRequirements() {
+		return Requirement.NO_REQUIREMENT;
+	}
 }
