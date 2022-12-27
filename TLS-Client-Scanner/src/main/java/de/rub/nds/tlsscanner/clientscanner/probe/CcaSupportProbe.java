0d70e03ea3745d5950867726ceb674b4c496724d
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 19:34:57 +0100

    adapted probe cca support and ciphersuite

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/CcaSupportProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/CcaSupportProbe.java
@@ -8,7 +8,9 @@
  */
 package de.rub.nds.tlsscanner.clientscanner.probe;
 
+import de.rub.nds.scanner.core.constants.TestResult;
 import de.rub.nds.scanner.core.constants.TestResults;
+import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.RunningModeType;
@@ -22,46 +24,45 @@ import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
+import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
-import de.rub.nds.tlsscanner.core.probe.result.CcaSupportResult;
 
-public class CcaSupportProbe
-        extends TlsClientProbe<ClientScannerConfig, ClientReport, CcaSupportResult<ClientReport>> {
+public class CcaSupportProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
 
-    public CcaSupportProbe(ParallelExecutor parallelExecutor, ClientScannerConfig scannerConfig) {
-        super(parallelExecutor, TlsProbeType.CCA_SUPPORT, scannerConfig);
-    }
+	private TestResult supportsCca;
 
-    @Override
-    public CcaSupportResult executeTest() {
-        Config config = scannerConfig.createConfig();
-        config.setClientAuthentication(true);
+	public CcaSupportProbe(ParallelExecutor parallelExecutor, ClientScannerConfig scannerConfig) {
+		super(parallelExecutor, TlsProbeType.CCA_SUPPORT, scannerConfig);
+		register(TlsAnalyzedProperty.SUPPORTS_CCA);
+	}
 
-        WorkflowTrace trace =
-                new WorkflowConfigurationFactory(config)
-                        .createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
-        trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+	@Override
+	public void executeTest() {
+		Config config = scannerConfig.createConfig();
+		config.setClientAuthentication(true);
 
-        State state = new State(config, trace);
-        executeState(state);
-        if (WorkflowTraceUtil.didReceiveMessage(
-                HandshakeMessageType.CERTIFICATE, state.getWorkflowTrace())) {
-            return new CcaSupportResult(TestResults.TRUE);
-        } else {
-            return new CcaSupportResult(TestResults.FALSE);
-        }
-    }
+		WorkflowTrace trace = new WorkflowConfigurationFactory(config).createWorkflowTrace(WorkflowTraceType.HELLO,
+				RunningModeType.SERVER);
+		trace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-    @Override
-    public boolean canBeExecuted(ClientReport report) {
-        return true;
-    }
+		State state = new State(config, trace);
+		executeState(state);
+		supportsCca = WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.CERTIFICATE, state.getWorkflowTrace())
+				? TestResults.TRUE
+				: TestResults.FALSE;
+	}
 
-    @Override
-    public CcaSupportResult getCouldNotExecuteResult() {
-        return new CcaSupportResult(TestResults.COULD_NOT_TEST);
-    }
+	@Override
+	public void adjustConfig(ClientReport report) {
+	}
 
-    @Override
-    public void adjustConfig(ClientReport report) {}
+	@Override
+	protected void mergeData(ClientReport report) {
+		put(TlsAnalyzedProperty.SUPPORTS_CCA, supportsCca);
+	}
+
+	@Override
+	protected Requirement getRequirements() {
+		return Requirement.NO_REQUIREMENT;
+	}
 }
