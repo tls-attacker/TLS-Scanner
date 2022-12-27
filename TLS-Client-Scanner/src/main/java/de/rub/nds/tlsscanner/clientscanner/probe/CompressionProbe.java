b4198c664a9c64f48336bc7f8482bc9a6109cca3
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2022-12-27 18:41:26 +0100

    adapted probe compression

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/CompressionProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/CompressionProbe.java
@@ -8,10 +8,9 @@
  */
 package de.rub.nds.tlsscanner.clientscanner.probe;
 
-import static de.rub.nds.tlsscanner.clientscanner.probe.TlsClientProbe.LOGGER;
-
 import de.rub.nds.scanner.core.constants.TestResult;
 import de.rub.nds.scanner.core.constants.TestResults;
+import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.CompressionMethod;
 import de.rub.nds.tlsattacker.core.constants.RunningModeType;
@@ -23,65 +22,77 @@ import de.rub.nds.tlsattacker.core.workflow.action.ReceiveTillAction;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowConfigurationFactory;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
-import de.rub.nds.tlsscanner.clientscanner.probe.result.CompressionResult;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
+import de.rub.nds.tlsscanner.core.constants.TlsAnalyzedProperty;
 import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
+import de.rub.nds.tlsscanner.core.probe.requirements.ProbeRequirement;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CompressionProbe
-        extends TlsClientProbe<ClientScannerConfig, ClientReport, CompressionResult> {
+public class CompressionProbe extends TlsClientProbe<ClientScannerConfig, ClientReport> {
 
-    private List<CompressionMethod> clientAdvertisedCompressions = null;
+	private List<CompressionMethod> supportedCompressions;
+	private TestResult forcedCompression;
 
-    public CompressionProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
-        super(executor, TlsProbeType.COMPRESSIONS, scannerConfig);
-    }
+	public CompressionProbe(ParallelExecutor executor, ClientScannerConfig scannerConfig) {
+		super(executor, TlsProbeType.COMPRESSIONS, scannerConfig);
+		register(TlsAnalyzedProperty.VULNERABLE_TO_CRIME, TlsAnalyzedProperty.SUPPORTS_TLS_COMPRESSION,
+				TlsAnalyzedProperty.FORCED_COMPRESSION, TlsAnalyzedProperty.SUPPORTED_COMPRESSION_METHODS);
+	}
 
-    @Override
-    public CompressionResult executeTest() {
-        List<CompressionMethod> supportedCompressions = new LinkedList<>();
-        for (CompressionMethod compressionMethod : CompressionMethod.values()) {
-            LOGGER.debug("Testing compression {}", compressionMethod);
+	@Override
+	public void executeTest() {
+		supportedCompressions = new LinkedList<>();
+		for (CompressionMethod compressionMethod : CompressionMethod.values()) {
+			LOGGER.debug("Testing compression {}", compressionMethod);
 
-            Config config = scannerConfig.createConfig();
-            config.setEnforceSettings(true);
-            config.setDefaultServerSupportedCompressionMethods(compressionMethod);
-            config.setDefaultSelectedCompressionMethod(compressionMethod);
+			Config config = scannerConfig.createConfig();
+			config.setEnforceSettings(true);
+			config.setDefaultServerSupportedCompressionMethods(compressionMethod);
+			config.setDefaultSelectedCompressionMethod(compressionMethod);
 
-            WorkflowConfigurationFactory factory = new WorkflowConfigurationFactory(config);
-            WorkflowTrace workflowTrace =
-                    factory.createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
-            workflowTrace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
+			WorkflowConfigurationFactory factory = new WorkflowConfigurationFactory(config);
+			WorkflowTrace workflowTrace = factory.createWorkflowTrace(WorkflowTraceType.HELLO, RunningModeType.SERVER);
+			workflowTrace.addTlsAction(new ReceiveTillAction(new FinishedMessage()));
 
-            State state = new State(config, workflowTrace);
-            executeState(state);
+			State state = new State(config, workflowTrace);
+			executeState(state);
 
-            if (state.getWorkflowTrace().executedAsPlanned()) {
-                supportedCompressions.add(compressionMethod);
-            }
-        }
-        TestResult forcedCompression;
-        if (!clientAdvertisedCompressions.containsAll(supportedCompressions)) {
-            forcedCompression = TestResults.TRUE;
-        } else {
-            forcedCompression = TestResults.FALSE;
-        }
-        return new CompressionResult(supportedCompressions, forcedCompression);
-    }
+			if (state.getWorkflowTrace().executedAsPlanned()) {
+				supportedCompressions.add(compressionMethod);
+			}
+		}
+		forcedCompression = !supportedCompressions.containsAll(supportedCompressions)
+				? forcedCompression = TestResults.TRUE
+				: TestResults.FALSE;
+	}
 
-    @Override
-    public boolean canBeExecuted(ClientReport report) {
-        return report.isProbeAlreadyExecuted(TlsProbeType.BASIC);
-    }
+	@Override
+	public void adjustConfig(ClientReport report) {
+		supportedCompressions = report.getClientAdvertisedCompressions();
+	}
 
-    @Override
-    public CompressionResult getCouldNotExecuteResult() {
-        return new CompressionResult(null, TestResults.COULD_NOT_TEST);
-    }
+	@Override
+	protected void mergeData(ClientReport report) {
+		if (supportedCompressions != null) {
+			put(TlsAnalyzedProperty.SUPPORTED_COMPRESSION_METHODS, supportedCompressions);
+			if (supportedCompressions.contains(CompressionMethod.LZS)
+					|| supportedCompressions.contains(CompressionMethod.DEFLATE)) {
+				put(TlsAnalyzedProperty.VULNERABLE_TO_CRIME, TestResults.TRUE);
+				put(TlsAnalyzedProperty.SUPPORTS_TLS_COMPRESSION, TestResults.TRUE);
+			} else {
+				put(TlsAnalyzedProperty.VULNERABLE_TO_CRIME, TestResults.FALSE);
+				put(TlsAnalyzedProperty.SUPPORTS_TLS_COMPRESSION, TestResults.FALSE);
+			}
+		} else {
+			put(TlsAnalyzedProperty.VULNERABLE_TO_CRIME, TestResults.COULD_NOT_TEST);
+			put(TlsAnalyzedProperty.SUPPORTS_TLS_COMPRESSION, TestResults.COULD_NOT_TEST);
+		}
+		put(TlsAnalyzedProperty.FORCED_COMPRESSION, forcedCompression);
+	}
 
-    @Override
-    public void adjustConfig(ClientReport report) {
-        clientAdvertisedCompressions = report.getClientAdvertisedCompressions();
-    }
+	@Override
+	protected Requirement getRequirements() {
+		return new ProbeRequirement(TlsProbeType.BASIC);
+	}
 }
