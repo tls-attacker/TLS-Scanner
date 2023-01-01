c30182a1bce264347496026b9b33fc4580e00f5b
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2023-01-01 23:43:46 +0100

    added javadoc for protocolversions requirement class

--- TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/probe/requirements/ProtocolRequirement.java
+++ TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/probe/requirements/ProtocolRequirement.java
@@ -15,55 +15,67 @@ import de.rub.nds.tlsscanner.core.report.TlsScanReport;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author shiji
+ *
+ *         Implements a {@link Requirement} of required supported
+ *         ProtocolVersions.
+ */
 public class ProtocolRequirement extends Requirement {
-    private final ProtocolVersion[] protocols;
-    private List<ProtocolVersion> missing;
+	private final ProtocolVersion[] protocols;
+	private List<ProtocolVersion> missing;
 
-    public ProtocolRequirement(ProtocolVersion... protocols) {
-        super();
-        this.protocols = protocols;
-        this.missing = new ArrayList<>();
-    }
+	/**
+	 * 
+	 * @param protocols the required ProtocolVersions. Any amount possible.
+	 */
+	public ProtocolRequirement(ProtocolVersion... protocols) {
+		super();
+		this.protocols = protocols;
+		this.missing = new ArrayList<>();
+	}
 
-    @Override
-    protected boolean evaluateIntern(ScanReport report) {
-        if ((protocols == null) || (protocols.length == 0)) {
-            return true;
-        }
-        boolean returnValue = false;
-        missing = new ArrayList<>();
-        List<ProtocolVersion> protocolVersions =
-                ((TlsScanReport) report).getSupportedProtocolVersions();
-        if (protocolVersions != null && !protocolVersions.isEmpty()) {
-            for (ProtocolVersion protocol : protocols) {
-                if (protocolVersions.contains(protocol)) {
-                    returnValue = true;
-                } else {
-                    missing.add(protocol);
-                }
-            }
-        } else {
-            for (ProtocolVersion protocol : protocols) {
-                missing.add(protocol);
-            }
-        }
-        return returnValue;
-    }
+	@Override
+	protected boolean evaluateIntern(ScanReport report) {
+		if ((protocols == null) || (protocols.length == 0)) {
+			return true;
+		}
+		boolean returnValue = false;
+		missing = new ArrayList<>();
+		List<ProtocolVersion> protocolVersions = ((TlsScanReport) report).getSupportedProtocolVersions();
+		if (protocolVersions != null && !protocolVersions.isEmpty()) {
+			for (ProtocolVersion protocol : protocols) {
+				if (protocolVersions.contains(protocol)) {
+					returnValue = true;
+				} else {
+					missing.add(protocol);
+				}
+			}
+		} else {
+			for (ProtocolVersion protocol : protocols) {
+				missing.add(protocol);
+			}
+		}
+		return returnValue;
+	}
 
-    public ProtocolVersion[] getRequirement() {
-        return protocols;
-    }
+	/**
+	 * 
+	 * @return the ProtocolVersions.
+	 */
+	public ProtocolVersion[] getRequirement() {
+		return protocols;
+	}
 
-    @Override
-    public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
-        if (evaluateIntern(report) == false) {
-            return next.getMissingRequirementIntern(
-                    missing.requires(
-                            new ProtocolRequirement(
-                                    this.missing.toArray(
-                                            new ProtocolVersion[this.missing.size()]))),
-                    report);
-        }
-        return next.getMissingRequirementIntern(missing, report);
-    }
+	@Override
+	public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
+		if (evaluateIntern(report) == false) {
+			return next.getMissingRequirementIntern(
+					missing.requires(
+							new ProtocolRequirement(this.missing.toArray(new ProtocolVersion[this.missing.size()]))),
+					report);
+		}
+		return next.getMissingRequirementIntern(missing, report);
+	}
 }
