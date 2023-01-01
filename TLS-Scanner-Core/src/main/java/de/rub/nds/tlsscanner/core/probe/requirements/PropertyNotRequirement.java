b8c55edd9371809efc1c4df413b7a053d08e4089
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2023-01-01 23:54:52 +0100

    added javadoc for properties and -not  requirement class

--- TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/probe/requirements/PropertyNotRequirement.java
+++ TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/probe/requirements/PropertyNotRequirement.java
@@ -17,53 +17,66 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 
+ * @author shiji
+ *
+ *         Represents a {@link Requirement} for required TlsAnalyzedProperties
+ *         which were negatively evaluated (TestResults.FALSE).
+ */
 public class PropertyNotRequirement extends Requirement {
 
-    private final TlsAnalyzedProperty[] propertiesNot;
-    private List<TlsAnalyzedProperty> missing;
+	private final TlsAnalyzedProperty[] propertiesNot;
+	private List<TlsAnalyzedProperty> missing;
 
-    public PropertyNotRequirement(TlsAnalyzedProperty... propertiesNot) {
-        super();
-        this.propertiesNot = propertiesNot;
-        this.missing = new ArrayList<>();
-    }
+	/**
+	 * 
+	 * @param properties the required negatively evaluated TlsAnalyzedProperties.
+	 *                   Any amount possible.
+	 */
+	public PropertyNotRequirement(TlsAnalyzedProperty... propertiesNot) {
+		super();
+		this.propertiesNot = propertiesNot;
+		this.missing = new ArrayList<>();
+	}
 
-    @Override
-    protected boolean evaluateIntern(ScanReport report) {
-        if ((propertiesNot == null) || (propertiesNot.length == 0)) {
-            return true;
-        }
-        boolean returnValue = true;
-        missing = new ArrayList<>();
-        Map<String, TestResult> propertyMap = report.getResultMap();
-        for (TlsAnalyzedProperty property : propertiesNot) {
-            if (propertyMap.containsKey(property.toString())) {
-                if (propertyMap.get(property.toString()) != TestResults.FALSE) {
-                    returnValue = false;
-                    missing.add(property);
-                }
-            } else {
-                returnValue = false;
-                missing.add(property);
-            }
-        }
-        return returnValue;
-    }
+	@Override
+	protected boolean evaluateIntern(ScanReport report) {
+		if ((propertiesNot == null) || (propertiesNot.length == 0)) {
+			return true;
+		}
+		boolean returnValue = true;
+		missing = new ArrayList<>();
+		Map<String, TestResult> propertyMap = report.getResultMap();
+		for (TlsAnalyzedProperty property : propertiesNot) {
+			if (propertyMap.containsKey(property.toString())) {
+				if (propertyMap.get(property.toString()) != TestResults.FALSE) {
+					returnValue = false;
+					missing.add(property);
+				}
+			} else {
+				returnValue = false;
+				missing.add(property);
+			}
+		}
+		return returnValue;
+	}
 
-    public TlsAnalyzedProperty[] getRequirement() {
-        return propertiesNot;
-    }
+	/**
+	 * 
+	 * @return the required negatively evaluated TlsAnalyzedProperties
+	 */
+	public TlsAnalyzedProperty[] getRequirement() {
+		return propertiesNot;
+	}
 
-    @Override
-    public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
-        if (evaluateIntern(report) == false) {
-            return next.getMissingRequirementIntern(
-                    missing.requires(
-                            new PropertyNotRequirement(
-                                    this.missing.toArray(
-                                            new TlsAnalyzedProperty[this.missing.size()]))),
-                    report);
-        }
-        return next.getMissingRequirementIntern(missing, report);
-    }
+	@Override
+	public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
+		if (evaluateIntern(report) == false) {
+			return next.getMissingRequirementIntern(missing.requires(
+					new PropertyNotRequirement(this.missing.toArray(new TlsAnalyzedProperty[this.missing.size()]))),
+					report);
+		}
+		return next.getMissingRequirementIntern(missing, report);
+	}
 }
--- TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/probe/requirements/PropertyRequirement.java
+++ TLS-Scanner-Core/src/main/java/de/rub/nds/tlsscanner/core/probe/requirements/PropertyRequirement.java
@@ -17,53 +17,65 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 
+ * @author shiji
+ *
+ *         Represents a {@link Requirement} for required TlsAnalyzedProperties
+ *         which were positively evaluated (TestResults.TRUE).
+ */
 public class PropertyRequirement extends Requirement {
 
-    private final TlsAnalyzedProperty[] properties;
-    private List<TlsAnalyzedProperty> missing;
+	private final TlsAnalyzedProperty[] properties;
+	private List<TlsAnalyzedProperty> missing;
 
-    public PropertyRequirement(TlsAnalyzedProperty... properties) {
-        super();
-        this.properties = properties;
-        this.missing = new ArrayList<>();
-    }
+	/**
+	 * 
+	 * @param properties the required TlsAnalyzedProperties. Any amount possible.
+	 */
+	public PropertyRequirement(TlsAnalyzedProperty... properties) {
+		super();
+		this.properties = properties;
+		this.missing = new ArrayList<>();
+	}
 
-    @Override
-    protected boolean evaluateIntern(ScanReport report) {
-        if ((properties == null) || (properties.length == 0)) {
-            return true;
-        }
-        boolean returnValue = true;
-        missing = new ArrayList<>();
-        Map<String, TestResult> propertyMap = report.getResultMap();
-        for (TlsAnalyzedProperty property : properties) {
-            if (propertyMap.containsKey(property.toString())) {
-                if (propertyMap.get(property.toString()) != TestResults.TRUE) {
-                    returnValue = false;
-                    missing.add(property);
-                }
-            } else {
-                returnValue = false;
-                missing.add(property);
-            }
-        }
-        return returnValue;
-    }
+	@Override
+	protected boolean evaluateIntern(ScanReport report) {
+		if ((properties == null) || (properties.length == 0)) {
+			return true;
+		}
+		boolean returnValue = true;
+		missing = new ArrayList<>();
+		Map<String, TestResult> propertyMap = report.getResultMap();
+		for (TlsAnalyzedProperty property : properties) {
+			if (propertyMap.containsKey(property.toString())) {
+				if (propertyMap.get(property.toString()) != TestResults.TRUE) {
+					returnValue = false;
+					missing.add(property);
+				}
+			} else {
+				returnValue = false;
+				missing.add(property);
+			}
+		}
+		return returnValue;
+	}
 
-    public TlsAnalyzedProperty[] getRequirement() {
-        return properties;
-    }
+	/**
+	 * 
+	 * @return the required TlsAnalyzedProperties.
+	 */
+	public TlsAnalyzedProperty[] getRequirement() {
+		return properties;
+	}
 
-    @Override
-    public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
-        if (evaluateIntern(report) == false) {
-            return next.getMissingRequirementIntern(
-                    missing.requires(
-                            new PropertyRequirement(
-                                    this.missing.toArray(
-                                            new TlsAnalyzedProperty[this.missing.size()]))),
-                    report);
-        }
-        return next.getMissingRequirementIntern(missing, report);
-    }
+	@Override
+	public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
+		if (evaluateIntern(report) == false) {
+			return next.getMissingRequirementIntern(missing.requires(
+					new PropertyRequirement(this.missing.toArray(new TlsAnalyzedProperty[this.missing.size()]))),
+					report);
+		}
+		return next.getMissingRequirementIntern(missing, report);
+	}
 }
