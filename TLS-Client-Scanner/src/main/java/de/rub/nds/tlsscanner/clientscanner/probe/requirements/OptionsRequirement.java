8efa8743899a6d3d1c358154c6c4aa35ac366c9a
Jia-Lin Shi <Jia-Lin.Shi@rub.de>
2023-01-04 11:20:52 +0100

    adapted options requirement to probe type as parameter

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/requirements/OptionsRequirement.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/requirements/OptionsRequirement.java
@@ -11,76 +11,79 @@ package de.rub.nds.tlsscanner.clientscanner.probe.requirements;
 import de.rub.nds.scanner.core.probe.requirements.Requirement;
 import de.rub.nds.scanner.core.report.ScanReport;
 import de.rub.nds.tlsscanner.clientscanner.config.ClientScannerConfig;
+import de.rub.nds.tlsscanner.core.constants.TlsProbeType;
 
-/** Represents a {@link Requirement} for additional, optional flags in commands. */
+/**
+ * Represents a {@link Requirement} for additional, optional flags in commands.
+ * Can be extended by adding a respective if clause in the evaluateIntern
+ * function.
+ */
 public class OptionsRequirement extends Requirement {
 
-    private ClientScannerConfig scannerConfig;
+	private ClientScannerConfig scannerConfig;
 
-    /** String name of the respective option. */
-    private String type;
+	/* Probetype of the respective option. */
+	private TlsProbeType type;
 
-    /** domain for sni option (optional). */
-    private String domain;
+	/* domain for sni option (optional). */
+	private String domain;
 
-    /**
-     * @param scannerConfig the ClientScannerConfig.
-     * @param type the name of the option.
-     */
-    public OptionsRequirement(ClientScannerConfig scannerConfig, String type) {
-        super();
-        this.scannerConfig = scannerConfig;
-        this.type = type;
-    }
+	/**
+	 * @param scannerConfig the ClientScannerConfig.
+	 * @param type          the name of the option.
+	 */
+	public OptionsRequirement(ClientScannerConfig scannerConfig, TlsProbeType type) {
+		super();
+		this.scannerConfig = scannerConfig;
+		this.type = type;
+	}
 
-    /**
-     * @param scannerConfig the ClientScannerConfig.
-     * @param type the name of the option.
-     * @param domain the domain for the sni option.
-     */
-    public OptionsRequirement(ClientScannerConfig scannerConfig, String type, String domain) {
-        super();
-        this.scannerConfig = scannerConfig;
-        this.type = type;
-        this.domain = domain;
-    }
+	/**
+	 * @param scannerConfig the ClientScannerConfig.
+	 * @param type          the name of the option.
+	 * @param domain        the domain for the sni option.
+	 */
+	public OptionsRequirement(ClientScannerConfig scannerConfig, TlsProbeType type, String domain) {
+		super();
+		this.scannerConfig = scannerConfig;
+		this.type = type;
+		this.domain = domain;
+	}
 
-    @Override
-    protected boolean evaluateIntern(ScanReport report) {
-        if (scannerConfig == null || type == null) {
-            return false;
-        }
-        if (type == "ALPN") {
-            return scannerConfig.getClientParameterDelegate().getAlpnOptions() != null;
-        }
+	@Override
+	protected boolean evaluateIntern(ScanReport report) {
+		if (scannerConfig == null || type == null) {
+			return false;
+		}
+		if (type == TlsProbeType.ALPN) {
+			return scannerConfig.getClientParameterDelegate().getAlpnOptions() != null;
+		}
+		if (type == TlsProbeType.SNI) {
+			if (domain != null) {
+				return scannerConfig.getClientParameterDelegate().getSniOptions(domain) != null;
+			} else {
+				return false;
+			}
+		}
+		if (type == TlsProbeType.RESUMPTION) {
+			return scannerConfig.getClientParameterDelegate().getResumptionOptions() != null;
+		}
+		return false;
+	}
 
-        // TODO is this correct for null domains?
-        if (type == "SNI") {
-            if (domain != null) {
-                return scannerConfig.getClientParameterDelegate().getSniOptions(domain) != null;
-            } else {
-                return false;
-            }
-        }
-        if (type == "RESUMPTION") {
-            return scannerConfig.getClientParameterDelegate().getResumptionOptions() != null;
-        }
-        return false;
-    }
+	/**
+	 * @return the probe type of the option.
+	 */
+	public TlsProbeType getRequirement() {
+		return type;
+	}
 
-    /**
-     * @return the name of the option.
-     */
-    public String getRequirement() {
-        return type;
-    }
-
-    @Override
-    public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
-        if (evaluateIntern(report) == false) {
-            return next.getMissingRequirementIntern(
-                    missing.requires(new OptionsRequirement(scannerConfig, type)), report);
-        }
-        return next.getMissingRequirementIntern(missing, report);
-    }
+	@Override
+	public Requirement getMissingRequirementIntern(Requirement missing, ScanReport report) {
+		if (evaluateIntern(report) == false) {
+			return next.getMissingRequirementIntern(missing.requires(new OptionsRequirement(scannerConfig, type)),
+					report);
+		}
+		return next.getMissingRequirementIntern(missing, report);
+	}
 }
