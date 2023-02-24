77697051165f3495e894a34cf7653c7946b48526
Jia-Lin <Jia-Lin.Shi@rub.de>
2023-02-25 00:53:04 +0100

    helper func to prot version

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ProtocolVersionProbe.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/probe/ProtocolVersionProbe.java
@@ -215,15 +215,7 @@ public class ProtocolVersionProbe extends TlsClientProbe<ClientScannerConfig, Cl
                 }
             }
         } else {
-            put(TlsAnalyzedProperty.SUPPORTS_DTLS_1_0_DRAFT, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_DTLS_1_0, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_DTLS_1_2, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_SSL_2, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_SSL_3, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_TLS_1_0, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_TLS_1_1, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_TLS_1_2, TestResults.COULD_NOT_TEST);
-            put(TlsAnalyzedProperty.SUPPORTS_TLS_1_3, TestResults.COULD_NOT_TEST);
+            setPropertiesToCouldNotTest();
         }
 
         put(TlsAnalyzedProperty.SUPPORTED_PROTOCOL_VERSIONS, supportedProtocolVersions);
