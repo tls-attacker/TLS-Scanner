6dc4597406a12c49e2539e000ce194aa44057f9e
S-e-c-C-o-d-i-n-g <105246301+S-e-c-C-o-d-i-n-g@users.noreply.github.com>
2024-09-25 18:26:52 +0200

    First implementation of guideline architecture and BSI guidelines

---
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/guideline/results/ExtensionGuidelineCheckResult.java
@@ -0,0 +1,44 @@
+/*
+ * TLS-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
+ *
+ * Copyright 2017-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsscanner.clientscanner.guideline.results;
+
+import de.rub.nds.scanner.core.guideline.GuidelineAdherence;
+import de.rub.nds.scanner.core.guideline.GuidelineCheckResult;
+import de.rub.nds.tlsattacker.core.constants.ExtensionType;
+
+public class ExtensionGuidelineCheckResult extends GuidelineCheckResult {
+
+    private final boolean supported;
+    private final ExtensionType requiredExtension;
+
+    public ExtensionGuidelineCheckResult(
+            String checkName,
+            GuidelineAdherence adherence,
+            boolean supported,
+            ExtensionType requiredExtension) {
+        super(checkName, adherence);
+        this.supported = supported;
+        this.requiredExtension = requiredExtension;
+    }
+
+    @Override
+    public String toString() {
+        return supported
+                ? "The client supports " + this.requiredExtension
+                : "The client does not support " + this.requiredExtension;
+    }
+
+    public ExtensionType getRequiredExtension() {
+        return requiredExtension;
+    }
+
+    public boolean isSupported() {
+        return supported;
+    }
+}
---
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/guideline/results/SignatureAndHashAlgorithmsCertificateGuidelineCheckResult.java
@@ -0,0 +1,47 @@
+/*
+ * TLS-Scanner - A TLS configuration and analysis tool based on TLS-Attacker
+ *
+ * Copyright 2017-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsscanner.clientscanner.guideline.results;
+
+import com.google.common.base.Joiner;
+import de.rub.nds.scanner.core.guideline.GuidelineAdherence;
+import de.rub.nds.scanner.core.guideline.GuidelineCheckResult;
+import de.rub.nds.tlsattacker.core.constants.SignatureAndHashAlgorithm;
+import java.util.Objects;
+import java.util.Set;
+
+public class SignatureAndHashAlgorithmsCertificateGuidelineCheckResult
+        extends GuidelineCheckResult {
+
+    private final Set<SignatureAndHashAlgorithm> notRecommendedAlgorithms;
+
+    public SignatureAndHashAlgorithmsCertificateGuidelineCheckResult(
+            String checkName,
+            GuidelineAdherence adherence,
+            Set<SignatureAndHashAlgorithm> notRecommendedAlgorithms) {
+        super(checkName, adherence);
+        this.notRecommendedAlgorithms = notRecommendedAlgorithms;
+    }
+
+    @Override
+    public String toString() {
+        if (Objects.equals(GuidelineAdherence.CHECK_FAILED, getAdherence())) {
+            return "Missing Information";
+        }
+        if (notRecommendedAlgorithms.isEmpty()) {
+            return "Only listed Signature and Hash Algorithms are supported.";
+        } else {
+            return "The following Signature and Hash Algorithms were supported but not recommended:\n"
+                    + Joiner.on('\n').join(notRecommendedAlgorithms);
+        }
+    }
+
+    public Set<SignatureAndHashAlgorithm> getNotRecommendedAlgorithms() {
+        return notRecommendedAlgorithms;
+    }
+}
