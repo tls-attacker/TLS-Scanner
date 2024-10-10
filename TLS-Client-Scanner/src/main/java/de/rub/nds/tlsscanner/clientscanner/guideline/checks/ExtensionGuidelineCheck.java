d6ef4cc49ba84039ad8a1b2ed3f49fb62cc9a63b
S-e-c-C-o-d-i-n-g <105246301+S-e-c-C-o-d-i-n-g@users.noreply.github.com>
2024-10-10 15:19:46 +0200

    Fixed ExtensionGuidelineCheck.java to consider RequirementLevel

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/guideline/checks/ExtensionGuidelineCheck.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/guideline/checks/ExtensionGuidelineCheck.java
@@ -47,9 +47,17 @@ public class ExtensionGuidelineCheck extends GuidelineCheck<ClientReport> {
 
     @Override
     public GuidelineCheckResult evaluate(ClientReport report) {
+        GuidelineAdherence adherence;
+
+        if (getRequirementLevel() == RequirementLevel.MUST_NOT || getRequirementLevel() == RequirementLevel.SHOULD_NOT) {
+            adherence = GuidelineAdherence.of(!report.getSupportedExtensions().contains(requiredExtension));
+        } else {
+            adherence = GuidelineAdherence.of(report.getSupportedExtensions().contains(requiredExtension));
+        }
+
         return new ExtensionGuidelineCheckResult(
                 getName(),
-                GuidelineAdherence.of(report.getSupportedExtensions().contains(requiredExtension)),
+                adherence,
                 report.getSupportedExtensions().contains(requiredExtension),
                 requiredExtension);
     }
