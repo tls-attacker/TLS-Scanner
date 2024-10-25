4589647280b289ba41ef4fee1264ee49c272658d
Robert Merget <robert.merget@tii.ae>
2024-10-25 10:28:14 +0400

    adjusted code to new interface

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/SignatureHashAlgorithmOrderProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/SignatureHashAlgorithmOrderProbe.java
@@ -40,11 +40,11 @@ public class SignatureHashAlgorithmOrderProbe extends TlsServerProbe {
     protected void executeTest() {
         List<SignatureAndHashAlgorithm> toTestList = new LinkedList<>();
         toTestList.addAll(Arrays.asList(SignatureAndHashAlgorithm.values()));
-        SignatureAndHashAlgorithm firstSelectedSignatureAndHashAlgorithm =
-                getSelectedSignatureAndHashAlgorithm(toTestList);
+        SignatureAndHashAlgorithm firstSelectedSignatureAndHashAlgorithm = getSelectedSignatureAndHashAlgorithm(
+                toTestList);
         Collections.reverse(toTestList);
-        SignatureAndHashAlgorithm secondSelectedSignatureAndHashAlgorithm =
-                getSelectedSignatureAndHashAlgorithm(toTestList);
+        SignatureAndHashAlgorithm secondSelectedSignatureAndHashAlgorithm = getSelectedSignatureAndHashAlgorithm(
+                toTestList);
 
         if (firstSelectedSignatureAndHashAlgorithm == secondSelectedSignatureAndHashAlgorithm) {
             enforced = TestResults.TRUE;
@@ -60,7 +60,8 @@ public class SignatureHashAlgorithmOrderProbe extends TlsServerProbe {
     }
 
     @Override
-    public void adjustConfig(ServerReport report) {}
+    public void adjustConfig(ServerReport report) {
+    }
 
     private SignatureAndHashAlgorithm getSelectedSignatureAndHashAlgorithm(
             List<SignatureAndHashAlgorithm> list) {
@@ -70,7 +71,7 @@ public class SignatureHashAlgorithmOrderProbe extends TlsServerProbe {
         config.setWorkflowTraceType(WorkflowTraceType.DYNAMIC_HELLO);
         State state = new State(config);
         executeState(state);
-        return state.getTlsContext().getSelectedSignatureAndHashAlgorithm();
+        return state.getTlsContext().getServerSelectedSignatureAndHashAlgorithm();
     }
 
     @Override
