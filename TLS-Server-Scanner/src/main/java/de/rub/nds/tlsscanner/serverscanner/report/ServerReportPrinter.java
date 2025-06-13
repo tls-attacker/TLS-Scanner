7f4419ad12ab061ac450ff2639eb1e9d65ca5f85
Karsten Meyer zu Selhausen <karsten.meyerzuselhausen@hackmanit.de>
2025-06-13 11:56:59 +0200

    Adjusted "weak" CS for AVOIDS_WEAK_CIPHER_SUITES property and formatting

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/report/ServerReportPrinter.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/report/ServerReportPrinter.java
@@ -1155,7 +1155,7 @@ public class ServerReportPrinter extends ReportPrinter<ServerReport> {
                 builder,
                 "Tickets resumable in different version",
                 TlsAnalyzedProperty.ALLOW_VERSION_CHANGE_TICKET);
-        prettyAppend(builder," "); // Workaround for single newline.
+        prettyAppend(builder, " "); // Workaround for single newline.
 
         prettyAppendSubheading(builder, "Details");
         // TODO use tables
@@ -2698,7 +2698,9 @@ public class ServerReportPrinter extends ReportPrinter<ServerReport> {
         prettyAppend(builder, "Violated: " + guidelineReport.getViolated().size(), AnsiColor.RED);
         prettyAppend(
                 builder, "Failed: " + guidelineReport.getFailedChecks().size(), AnsiColor.YELLOW);
-        prettyAppend(builder, "Condition Not Met: " + guidelineReport.getConditionNotMet().size() + "\n");
+        prettyAppend(
+                builder,
+                "Condition Not Met: " + guidelineReport.getConditionNotMet().size() + "\n");
         if (this.detail.isGreaterEqualTo(ScannerDetail.DETAILED)) {
             prettyAppend(builder, StringUtils.trim(guidelineReport.getLink()), AnsiColor.BLUE);
 
