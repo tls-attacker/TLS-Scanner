71a3989c2ae00a0b2fa40ee7c432df53b138f2ba
Robert Merget <robert.merget@tii.ae>
2025-06-16 12:27:40 +0400

    reverted version change and removed unused imports

--- TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/Main.java
+++ TLS-Client-Scanner/src/main/java/de/rub/nds/tlsscanner/clientscanner/Main.java
@@ -18,10 +18,8 @@ import de.rub.nds.tlsscanner.clientscanner.execution.TlsClientScanner;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientContainerReportCreator;
 import de.rub.nds.tlsscanner.clientscanner.report.ClientReport;
 import java.io.IOException;
-import java.security.Security;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
 
--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/Main.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/Main.java
@@ -19,10 +19,8 @@ import de.rub.nds.tlsscanner.serverscanner.execution.TlsServerScanner;
 import de.rub.nds.tlsscanner.serverscanner.report.ServerReport;
 import de.rub.nds.tlsscanner.serverscanner.report.ServerReportPrinter;
 import java.io.IOException;
-import java.security.Security;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class Main {
 
