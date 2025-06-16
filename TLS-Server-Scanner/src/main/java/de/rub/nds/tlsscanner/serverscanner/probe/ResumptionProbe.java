6585eb9d0c5286d2ddee32cda6f86e27a0d3268d
Karsten Meyer zu Selhausen <karsten.meyerzuselhausen@hackmanit.de>
2025-06-16 13:19:33 +0200

    Log warning instead of error for PSK mode in ResumptionProbe

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/ResumptionProbe.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/ResumptionProbe.java
@@ -361,7 +361,7 @@ public class ResumptionProbe extends TlsServerProbe {
                 throw new RuntimeException(e);
             } else {
                 if (exchangeModes.size() > 1) {
-                    LOGGER.error(
+                    LOGGER.warn(
                             "Could not test if the server prefers PSK_DHE_KE as the TLS 1.3 PSK key exchange mode (provided: "
                                     + exchangeModes.toString()
                                     + "): ",
