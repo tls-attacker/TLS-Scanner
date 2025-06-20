9b8e79feaea3abd68dce5abe7fbb524e166e2771
Robert Merget <robert.merget@tii.ae>
2025-06-20 16:13:41 +0000

    Add Javadoc to HpkpPin class
    Added comprehensive Javadoc comments to all public methods in the HpkpPin class:
    - Constructor documentation with parameter descriptions
    - Getter and setter method documentation
    - toString() method documentation

--- TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/result/hpkp/HpkpPin.java
+++ TLS-Server-Scanner/src/main/java/de/rub/nds/tlsscanner/serverscanner/probe/result/hpkp/HpkpPin.java
@@ -19,27 +19,58 @@ public class HpkpPin {
     @SuppressWarnings("unused")
     private HpkpPin() {}
 
+    /**
+     * Constructs a new HpkpPin with the specified pin type and pin value.
+     *
+     * @param pinType the type of the pin (e.g., "sha256")
+     * @param ping the pin value as a byte array
+     */
     public HpkpPin(String pinType, byte[] ping) {
         this.pinType = pinType;
         this.pin = ping;
     }
 
+    /**
+     * Returns the type of the pin.
+     *
+     * @return the pin type (e.g., "sha256")
+     */
     public String getPinType() {
         return pinType;
     }
 
+    /**
+     * Sets the type of the pin.
+     *
+     * @param pinType the pin type to set (e.g., "sha256")
+     */
     public void setPinType(String pinType) {
         this.pinType = pinType;
     }
 
+    /**
+     * Returns the pin value as a byte array.
+     *
+     * @return the pin value
+     */
     public byte[] getPing() {
         return pin;
     }
 
+    /**
+     * Sets the pin value.
+     *
+     * @param ping the pin value to set as a byte array
+     */
     public void setPing(byte[] ping) {
         this.pin = ping;
     }
 
+    /**
+     * Returns a string representation of this HpkpPin.
+     *
+     * @return a string containing the pin type and pin value in hexadecimal format
+     */
     @Override
     public String toString() {
         return ""
